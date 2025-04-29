09e1e82335b69d7963997fad9aba4b9f15ff390a
Ruben Silva <rubendacostaesilva@gmail.com>
2025-04-29 14:15:36 +0100

    Reduce top marging

--- src/components/wrappers/PageWrapper.tsx
+++ src/components/wrappers/PageWrapper.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function PageWrapper({ children }: React.PropsWithChildren) {
   return (
-    <main className="mt-16 flex-1">
+    <main className="flex-1 mt-8">
       <div className="container">{children}</div>
     </main>
   );
