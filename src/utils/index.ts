d277707ebfb086215192bc4c82ef4772fa3dd5d0
Ruben Silva <rubendacostaesilva@gmail.com>
2025-05-02 14:58:21 +0100

    Create utility functions
    - for validating if requests params are null, and sending a message that will describe which params are missing

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,3 +9,27 @@ export const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+export const checkIfQueryParamsIsValid = (params: Record<string, any>) =>
+  Object.values(params).every((value) => {
+    if (value === null) {
+      return false;
+    }
+    return true;
+  });
+
+export const getInvalidParamsMessage = (params: Record<string, any>) => {
+  const invalidParams: string[] = [];
+
+  for (const [key, value] of Object.entries(params)) {
+    if (value === null) {
+      invalidParams.push(key);
+    }
+  }
+
+  invalidParams.length === 0 && invalidParams.push("nenhum");
+
+  return `os seguintes parâmetros não foram submetidos: ${invalidParams.join(
+    ", "
+  )}`;
+};
