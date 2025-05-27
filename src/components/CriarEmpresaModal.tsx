27453498f82f740aea22fd0bf77c823656e1d51e
Ruben Silva <rubendacostaesilva@gmail.com>
2025-05-27 17:05:38 +0100

    Create component

---
+++ src/components/CriarEmpresaModal.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+import { Modal } from "@mantine/core";
+
+export default function CriarEmpresaModal() {
+  return (
+    <Modal opened={true} onClose={() => {}}>
+      Criar Empresa Modal
+    </Modal>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Grid } from "./Grid";
 export { default as GridCard } from "./GridCard";
 export { default as NewGridItem } from "./NewGridItem";
 export { default as EmpresaWidget } from "./EmpresaWidget";
+export { default as CriarEmpresaModal } from "./CriarEmpresaModal";
