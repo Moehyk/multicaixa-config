ea86345e7ccfff82c3e8b0e3b65e75396fe9a9c7
Ruben Silva <rubendacostaesilva@gmail.com>
2025-05-30 13:05:37 +0100

    Update component
    - setup styles for modal body, footer section and buttons

--- src/components/empresa/EmpresaModalForm.tsx
+++ src/components/empresa/EmpresaModalForm.tsx
@@ -13,87 +13,93 @@ export default function EmpresaModalForm() {
   } = useEmpresaModalForm();
 
   return (
-    <div className="p-2">
-      <Stepper active={active} className="h-88">
-        <Stepper.Step label="Dados da Empresa">
-          <div className="flex gap-2">
+    <>
+      <div className="p-6">
+        <Stepper active={active} className="h-88">
+          <Stepper.Step label="Dados da Empresa">
+            <div className="flex gap-2">
+              <TextInput
+                {...getInputProps("nome")}
+                label="Nome"
+                className="flex-1"
+              />
+              <TextInput {...getInputProps("sigla")} label="Sigla" />
+            </div>
+            <div className="flex gap-2">
+              <TextInput
+                {...getInputProps("morada")}
+                label="Morada"
+                className="flex-1"
+              />
+              <TextInput {...getInputProps("localidade")} label="Localidade" />
+            </div>
+            <div className="flex gap-2">
+              <TextInput
+                {...getInputProps("numero_pessoa_colectiva")}
+                label="Número Pessoa Colectiva"
+                className="flex-1"
+              />
+              <TextInput
+                {...getInputProps("cae")}
+                label="CAE"
+                className="flex-1"
+              />
+            </div>
+          </Stepper.Step>
+          <Stepper.Step label="Contato da Empresa">
+            <TextInput {...getInputProps("responsavel")} label="Responsável" />
+            <div className="flex gap-2">
+              <TextInput
+                {...getInputProps("telefone")}
+                label="Telefone"
+                className="flex-1"
+              />
+              <TextInput
+                {...getInputProps("email")}
+                label="Email"
+                className="flex-1"
+              />
+            </div>
+          </Stepper.Step>
+          <Stepper.Step label="Desigação no Sistema">
             <TextInput
-              {...getInputProps("nome")}
-              label="Nome"
-              className="flex-1"
-            />
-            <TextInput {...getInputProps("sigla")} label="Sigla" />
-          </div>
-          <div className="flex gap-2">
-            <TextInput
-              {...getInputProps("morada")}
-              label="Morada"
-              className="flex-1"
-            />
-            <TextInput {...getInputProps("localidade")} label="Localidade" />
-          </div>
-          <div className="flex gap-2">
-            <TextInput
-              {...getInputProps("numero_pessoa_colectiva")}
-              label="Número Pessoa Colectiva"
-              className="flex-1"
+              {...getInputProps("numero_entidade")}
+              label="Número Entidade"
+              placeholder="Número Entidade"
             />
             <TextInput
-              {...getInputProps("cae")}
-              label="CAE"
-              className="flex-1"
-            />
-          </div>
-        </Stepper.Step>
-        <Stepper.Step label="Contato da Empresa">
-          <TextInput {...getInputProps("responsavel")} label="Responsável" />
-          <div className="flex gap-2">
-            <TextInput
-              {...getInputProps("telefone")}
-              label="Telefone"
-              className="flex-1"
+              {...getInputProps("desig_ecra")}
+              label="Designação p/ ecrã"
+              placeholder="Nome que irá aparecer no ecrã Multicaixa"
             />
             <TextInput
-              {...getInputProps("email")}
-              label="Email"
-              className="flex-1"
+              {...getInputProps("desig_tecla_seleccao")}
+              label="Designação p/ tecla de selecção"
+              placeholder="Nome para a tecla de selecção no Multicaixa"
             />
-          </div>
-        </Stepper.Step>
-        <Stepper.Step label="Desigação no Sistema">
-          <TextInput
-            {...getInputProps("numero_entidade")}
-            label="Número Entidade"
-            placeholder="Número Entidade"
-          />
-          <TextInput
-            {...getInputProps("desig_ecra")}
-            label="Designação p/ ecrã"
-            placeholder="Nome que irá aparecer no ecrã Multicaixa"
-          />
-          <TextInput
-            {...getInputProps("desig_tecla_seleccao")}
-            label="Designação p/ tecla de selecção"
-            placeholder="Nome para a tecla de selecção no Multicaixa"
-          />
-        </Stepper.Step>
-        <Stepper.Completed>
-          Form Values: {JSON.stringify(getValues())}
-        </Stepper.Completed>
-      </Stepper>
-      <div className="flex w-full mt-4">
-        {active !== 0 && <Button onClick={prevStep}>Voltar</Button>}
+          </Stepper.Step>
+          <Stepper.Completed>
+            Form Values: {JSON.stringify(getValues())}
+          </Stepper.Completed>
+        </Stepper>
+      </div>
+      <div className="flex w-full mt-4 bg-modal-footer p-6 border-t border-modal-footer-b">
+        {active !== 0 && (
+          <Button variant="default" onClick={prevStep}>
+            Voltar
+          </Button>
+        )}
         {active !== 3 && (
-          <Button onClick={nextStep} className="ml-auto">
+          <Button variant="default" onClick={nextStep} className="ml-auto">
             Próximo
           </Button>
         )}
         {active === 3 && (
-          <Button onClick={() => {}} className="ml-auto">
+          <Button color="green" onClick={() => {}} className="ml-auto">
             Finalizar
           </Button>
         )}
       </div>
-    </div>
+    </>
   );
 }
