6674cc9be61a2b0079afe32236fbf09d12b52d1b
Ruben Silva <rubendacostaesilva@gmail.com>
2025-05-30 11:46:57 +0100

    Update empresa schemas

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -1,20 +1,56 @@
 import { z } from "zod";
 
+const nome = z.string().min(1, { message: "Campo obrigatório." });
+const sigla = z.string().min(1, { message: "Campo obrigatório." });
+const morada = z.string().min(1, { message: "Campo obrigatório." });
+const localidade = z.string().min(1, { message: "Campo obrigatório." });
+const cae = z.string().min(5, { message: "Número CAE inválido." });
+const numero_pessoa_colectiva = z
+  .string()
+  .min(10, { message: "Número Pessoa Colectiva inválido." });
+
+const responsavel = z.string().min(1, { message: "Campo obrigatório." });
+const telefone = z
+  .string()
+  .startsWith("9")
+  .length(9, { message: "Número de telemóvel inválido." });
+const email = z.string().email({ message: "Email inválido." });
+const numero_entidade = z.string().min(3);
+const desig_ecra = z.string().min(3);
+const desig_tecla_seleccao = z.string().min(3);
+
+export const empresaStepOneSchema = z.object({
+  nome,
+  sigla,
+  morada,
+  localidade,
+  cae,
+  numero_pessoa_colectiva,
+});
+
+export const empresaStepTwoSchema = z.object({
+  responsavel,
+  telefone,
+  email,
+});
+
+export const empresaStepThreeSchema = z.object({
+  numero_entidade,
+  desig_ecra,
+  desig_tecla_seleccao,
+});
+
 export const empresaSchema = z.object({
-  nome: z.string().min(1, { message: "Campo obrigatório." }),
-  sigla: z.string().min(1, { message: "Campo obrigatório." }),
-  morada: z.string().min(1, { message: "Campo obrigatório." }),
-  localidade: z.string().min(1, { message: "Campo obrigatório." }),
-  responsavel: z.string().min(1, { message: "Campo obrigatório." }),
-  telefone: z
-    .string()
-    .startsWith("9")
-    .length(9, { message: "Número de telemóvel inválido." }),
-  email: z.string().email({ message: "Email inválido." }),
-  cae: z.string().min(3),
-  numero_pessoa_colectiva: z.string().min(3),
-  logo: z.string().optional(),
-  numero_entidade: z.string().min(3),
-  desig_ecra: z.string().min(3),
-  desig_tecla_seleccao: z.string().min(3),
+  nome,
+  sigla,
+  morada,
+  localidade,
+  cae,
+  numero_pessoa_colectiva,
+  responsavel,
+  telefone,
+  email,
+  numero_entidade,
+  desig_ecra,
+  desig_tecla_seleccao,
 });
