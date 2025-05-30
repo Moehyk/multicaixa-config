6f552841746bfe3cbdaf463b709a66908e6f04dc
Ruben Silva <rubendacostaesilva@gmail.com>
2025-05-30 14:08:07 +0100

    Update component
    - added key to inputs;
    
    - created ConfirmSection component, to display form values;
    
    - added Modal component as parent, so that we could close it after form submission;
    
    - added submit form funcitonality, with lloading and error handling.

--- src/components/empresa/EmpresaModalForm.tsx
+++ src/components/empresa/EmpresaModalForm.tsx
@@ -1,61 +1,134 @@
 "use client";
 
-import { useEmpresaModalForm } from "@/hooks/useEmpresaForm";
+import { createEmpresa } from "@/server/services";
+import { useEmpresaModalForm, useFormMutation } from "@/hooks";
+import { errorNotification, sucessNotification } from "@/utils/notifications";
 
-import { Stepper, TextInput, Button } from "@mantine/core";
+import { Stepper, TextInput, Button, Modal } from "@mantine/core";
+
+import type { EmpresaForm } from "@/types";
+
+function ConfirmSection({ values }: { values: EmpresaForm }) {
+  return (
+    <div className="rounded-sm p-2 grid grid-cols-2 grid-flow-row gap-2">
+      <p className="font-medium">Nome</p>
+      {values.nome}
+      <p className="font-medium">Sigla</p>
+      {values.sigla}
+      <p className="font-medium">Morada</p>
+      {values.morada}
+      <p className="font-medium">Localidade</p>
+      {values.localidade}
+      <p className="font-medium">Responsável</p>
+      {values.responsavel}
+      <p className="font-medium">Telefone</p>
+      {values.telefone}
+      <p className="font-medium">Email</p>
+      {values.email}
+      <p className="font-medium">Número Entidade</p>
+      {values.numero_entidade}
+      <p className="font-medium">Designação p/ ecrã</p>
+      {values.desig_ecra}
+      <p className="font-medium">Designação p/ tecla de selecção</p>
+      {values.desig_tecla_seleccao}
+    </div>
+  );
+}
 
 export default function EmpresaModalForm() {
   const {
     active,
     nextStep,
     prevStep,
-    form: { getInputProps, getValues },
+    close,
+    opened,
+    form: { getInputProps, values, key },
   } = useEmpresaModalForm();
 
+  const { isMutating, setIsFetching, startTransition, push, back } =
+    useFormMutation();
+
+  const handleSubmit = async () => {
+    setIsFetching(true);
+
+    const response = await createEmpresa(values);
+
+    setIsFetching(false);
+    if (response.status !== 200) {
+      errorNotification(response);
+    }
+
+    close();
+  };
+
   return (
-    <>
+    <Modal
+      opened={opened}
+      withCloseButton={false}
+      title="Criar Empresa"
+      size="xl"
+      onClose={() => {}}
+    >
       <div className="p-6">
-        <Stepper active={active} className="h-88">
+        <Stepper active={active} className="h-96">
           <Stepper.Step label="Dados da Empresa">
             <div className="flex gap-2">
               <TextInput
                 {...getInputProps("nome")}
+                key={key("nome")}
                 label="Nome"
                 className="flex-1"
               />
-              <TextInput {...getInputProps("sigla")} label="Sigla" />
+              <TextInput
+                {...getInputProps("sigla")}
+                key={key("sigla")}
+                label="Sigla"
+              />
             </div>
             <div className="flex gap-2">
               <TextInput
                 {...getInputProps("morada")}
+                key={key("morada")}
                 label="Morada"
                 className="flex-1"
               />
-              <TextInput {...getInputProps("localidade")} label="Localidade" />
+              <TextInput
+                {...getInputProps("localidade")}
+                key={key("localidade")}
+                label="Localidade"
+              />
             </div>
             <div className="flex gap-2">
               <TextInput
                 {...getInputProps("numero_pessoa_colectiva")}
+                key={key("numero_pessoa_colectiva")}
                 label="Número Pessoa Colectiva"
                 className="flex-1"
               />
               <TextInput
                 {...getInputProps("cae")}
+                key={key("cae")}
                 label="CAE"
                 className="flex-1"
               />
             </div>
           </Stepper.Step>
           <Stepper.Step label="Contato da Empresa">
-            <TextInput {...getInputProps("responsavel")} label="Responsável" />
+            <TextInput
+              {...getInputProps("responsavel")}
+              key={key("responsavel")}
+              label="Responsável"
+            />
             <div className="flex gap-2">
               <TextInput
                 {...getInputProps("telefone")}
+                key={key("telefone")}
                 label="Telefone"
                 className="flex-1"
               />
               <TextInput
                 {...getInputProps("email")}
+                key={key("email")}
                 label="Email"
                 className="flex-1"
               />
@@ -64,22 +137,25 @@ export default function EmpresaModalForm() {
           <Stepper.Step label="Desigação no Sistema">
             <TextInput
               {...getInputProps("numero_entidade")}
+              key={key("numero_entidade")}
               label="Número Entidade"
               placeholder="Número Entidade"
             />
             <TextInput
               {...getInputProps("desig_ecra")}
+              key={key("desig_ecra")}
               label="Designação p/ ecrã"
               placeholder="Nome que irá aparecer no ecrã Multicaixa"
             />
             <TextInput
               {...getInputProps("desig_tecla_seleccao")}
+              key={key("desig_tecla_seleccao")}
               label="Designação p/ tecla de selecção"
               placeholder="Nome para a tecla de selecção no Multicaixa"
             />
           </Stepper.Step>
           <Stepper.Completed>
-            Form Values: {JSON.stringify(getValues())}
+            <ConfirmSection values={values} />
           </Stepper.Completed>
         </Stepper>
       </div>
@@ -95,11 +171,16 @@ export default function EmpresaModalForm() {
           </Button>
         )}
         {active === 3 && (
-          <Button color="green" onClick={() => {}} className="ml-auto">
+          <Button
+            color="green"
+            onClick={handleSubmit}
+            loading={isMutating}
+            className="ml-auto"
+          >
             Finalizar
           </Button>
         )}
       </div>
-    </>
+    </Modal>
   );
 }
