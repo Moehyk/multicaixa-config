d5a4c36e93c5f6f78db525ef501839f2f47e086e
Ruben Silva <rubendacostaesilva@gmail.com>
2025-05-30 18:35:56 +0100

    Update component
    - restructured markup;
    
    - setup server action, with loading state, notification and page transisiton

--- src/components/empresa/EmpresaForm.tsx
+++ src/components/empresa/EmpresaForm.tsx
@@ -4,120 +4,113 @@ import { updateEmpresa } from "@/server/services";
 import { useFormMutation, useEmpresaForm } from "@/hooks";
 import { errorNotification, sucessNotification } from "@/utils/notifications";
 
+import Link from "next/link";
 import { Button, TextInput } from "@mantine/core";
 
 import type { EmpresaForm } from "@/types";
+import { Empresa } from "@prisma/client";
 
-export default function EmpresaForm({ id }: { id: string | undefined }) {
-  const { getInputProps, onSubmit } = useEmpresaForm();
+export default function EmpresaForm(data: Empresa) {
+  const { getInputProps, onSubmit, setInitialValues, setValues } =
+    useEmpresaForm(data);
   const { isMutating, setIsFetching, startTransition, push, back } =
     useFormMutation();
 
-  const handleSubmit = async (values: EmpresaForm) => {
-    console.log("values", values);
-    //setIsFetching(true);
-    const response = await updateEmpresa(values, id);
-    //setIsFetching(false);
+  const handleSubmit = async (values: Empresa) => {
+    setIsFetching(true);
+    const response = await updateEmpresa(values);
+    setIsFetching(false);
 
-    console.log("response", response);
+    if (response.status !== 200) {
+      errorNotification(response);
+    }
 
-    console.log("values", values);
+    sucessNotification(response);
+    startTransition(() => push("/multicaixa"));
   };
 
   return (
-    <form className="flex flex-col mt-4" onSubmit={onSubmit(handleSubmit)}>
-      <div className="flex gap-4">
-        <TextInput
-          {...getInputProps("sigla")}
-          label="Sigla"
-          placeholder="Sigla da empresa"
-          className="flex-1"
-        />
-        <TextInput
-          {...getInputProps("numero_pessoa_colectiva")}
-          label="Número Pessoa Colectiva"
-          placeholder="Número Pessoa Colectiva"
-          className="flex-1 h-24"
-        />
-        <TextInput
-          {...getInputProps("cae")}
-          label="CAE"
-          placeholder="CAE da Empresa"
-          className="flex-1 h-24"
-        />
+    <form className="flex flex-col" onSubmit={onSubmit(handleSubmit)}>
+      <div className="px-8">
+        <div className="flex gap-4">
+          <TextInput
+            {...getInputProps("sigla")}
+            label="Sigla"
+            className="flex-1"
+          />
+          <TextInput
+            {...getInputProps("numero_pessoa_colectiva")}
+            label="Número Pessoa Colectiva"
+            className="flex-1 h-24"
+          />
+          <TextInput
+            {...getInputProps("cae")}
+            label="CAE"
+            className="flex-1 h-24"
+          />
+        </div>
+        <TextInput {...getInputProps("nome")} label="Nome" />
+        <div className="flex items-end gap-4">
+          <TextInput
+            {...getInputProps("morada")}
+            label="Morada"
+            className="flex-1"
+          />
+          <TextInput
+            {...getInputProps("localidade")}
+            label="Localidade"
+            className="w-1/4"
+          />
+        </div>
+        <div className="flex items-end gap-4">
+          <TextInput
+            {...getInputProps("responsavel")}
+            label="Responsável"
+            className="flex-1"
+          />
+          <TextInput
+            {...getInputProps("telefone")}
+            label="Telefone"
+            className="w-1/4"
+          />
+          <TextInput
+            {...getInputProps("email")}
+            label="Email"
+            className="w-1/4"
+          />
+        </div>
+        <div className="flex items-end gap-4">
+          <TextInput
+            {...getInputProps("numero_entidade")}
+            label="Número Entidade"
+            className="w-1/4"
+          />
+          <TextInput
+            {...getInputProps("desig_ecra")}
+            label="Designação p/ ecrã"
+            className="flex-1"
+          />
+          <TextInput
+            {...getInputProps("desig_tecla_seleccao")}
+            label="Designação p/ tecla de selecção"
+            className="flex-1"
+          />
+        </div>
       </div>
-      <TextInput
-        {...getInputProps("nome")}
-        label="Nome"
-        placeholder="Nome da Empresa"
-      />
-      <div className="flex items-end gap-4">
-        <TextInput
-          {...getInputProps("morada")}
-          label="Morada"
-          placeholder="Morada da Empresa"
-          className="flex-1"
-        />
-        <TextInput
-          {...getInputProps("localidade")}
-          label="Localidade"
-          placeholder="Localidade da Empresa"
-          className="w-1/4"
-        />
+      <div className="flex gap-2 bg-modal-footer p-8 border-t border-modal-footer-b">
+        <Button
+          component={Link}
+          href="/multicaixa"
+          variant="default"
+          size="lg"
+          type="submit"
+        >
+          Voltar
+        </Button>
+        <Button size="lg" type="submit" loading={isMutating}>
+          Salvar
+        </Button>
       </div>
-      <div className="flex items-end gap-4">
-        <TextInput
-          {...getInputProps("responsavel")}
-          label="Responsável"
-          placeholder="Responsável pela Empresa"
-          className="flex-1"
-        />
-        <TextInput
-          {...getInputProps("telefone")}
-          label="Telefone"
-          placeholder="Telefone da Empresa"
-          className="w-1/4"
-        />
-        <TextInput
-          {...getInputProps("email")}
-          label="Email"
-          placeholder="Email da Empresa"
-          className="w-1/4"
-        />
-      </div>
-      <TextInput
-        {...getInputProps("logo")}
-        label="Logo"
-        placeholder="Url para o logo da empresa"
-      />
-      <div className="flex items-end gap-4">
-        <TextInput
-          {...getInputProps("numero_entidade")}
-          label="Número Entidade"
-          placeholder="Número Entidade"
-          className="w-1/4"
-        />
-        <TextInput
-          {...getInputProps("desig_ecra")}
-          label="Designação p/ ecrã"
-          placeholder="Nome que irá aparecer no ecrã Multicaixa"
-          className="flex-1"
-        />
-        <TextInput
-          {...getInputProps("desig_tecla_seleccao")}
-          label="Designação p/ tecla de selecção"
-          placeholder="Nome para a tecla de selecção no Multicaixa"
-          className="flex-1"
-        />
-      </div>
-      <Button
-        size="lg"
-        type="submit"
-        className="max-w-fit mt-2"
-        loading={isMutating}
-      >
-        Salvar
-      </Button>
     </form>
   );
 }
