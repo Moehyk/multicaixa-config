12fe49f4c28aa0b5093e8df759fe53805c21f906
Ruben Silva <rubendacostaesilva@gmail.com>
2025-06-02 10:49:14 +0100

    Update component
    - added flexbox to CSS

--- src/components/wrappers/PageWrapper.tsx
+++ src/components/wrappers/PageWrapper.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function PageWrapper({ children }: React.PropsWithChildren) {
   return (
     <main className="flex-1 mt-8">
-      <div className="container">{children}</div>
+      <div className="container flex flex-col">{children}</div>
     </main>
   );
 }
