a1e72ea66b0d6eee0ae6aa120cd16c23584131f8
Ruben Silva <rubendacostaesilva@gmail.com>
2025-06-05 16:12:59 +0100

    Delete service

--- src/server/services/index.ts
+++ src/server/services/index.ts
@@ -17,19 +17,6 @@ export const getEmpresa = async () => {
   return response;
 };
 
-export const initEmpresa = async () => {
-  let empresaExist = false;
-
-  // Check if empresa exists
-  const { status, data: empresa } = await api.empresa.get();
-
-  if (status === 200) {
-    empresaExist = true;
-  }
-
-  return { empresaExist, empresa };
-};
-
 export const createEmpresa = async (values: Empresa) => {
   const response = await api.empresa.create(values);
 
