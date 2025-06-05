78207728c7879831b3f33b9810bd36fd7806a407
Ruben Silva <rubendacostaesilva@gmail.com>
2025-06-05 17:15:01 +0100

    Update component
    - removed unused stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import { checkIfQueryParamsIsValid, getInvalidParamsMessage } from "@/utils";
-
 import { Empresa } from "@prisma/client";
 
 const empresa: Empresa = {
@@ -14,7 +12,6 @@ const empresa: Empresa = {
   responsavel: "Responsavel",
   telefone: "null",
   email: "empresa@empresa.com",
-  logo: "https://via.placeholder.com/150",
   numero_entidade: "1",
   desig_ecra: "1234567890",
   desig_tecla_seleccao: "1234567890",
@@ -23,16 +20,5 @@ const empresa: Empresa = {
 import { Anchor } from "@mantine/core";
 
 export default function Home() {
-  const isValid = checkIfQueryParamsIsValid(empresa);
-
-  console.log("isValid", isValid);
-
-  if (!isValid) {
-    const message = getInvalidParamsMessage(empresa);
-
-    console.log("siga para bingo: ", isValid, message);
-  } else {
-    console.log("siga para bingo: ", isValid);
-  }
   return <Anchor href="/multicaixa">Multicaixa</Anchor>;
 }
