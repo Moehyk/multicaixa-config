d14160671b16c40768a41800a53dbd487092d4bf
Ruben Silva <rubendacostaesilva@gmail.com>
2025-06-05 12:36:13 +0100

    Update component
    - structure JSX with needed components
    
    - setup hooks for form validation
    
    - setup submit form functionality, with loading and notifications

--- src/components/servico/ServicoModalForm.tsx
+++ src/components/servico/ServicoModalForm.tsx
@@ -1,22 +1,65 @@
-import React from "react";
+import { createServico } from "@/server/services";
+import { modals } from "@mantine/modals";
+import { useServicoForm, useFormMutation } from "@/hooks";
+import { errorNotification, sucessNotification } from "@/utils/notifications";
 
-import { Button } from "@mantine/core";
+import { Button, TextInput } from "@mantine/core";
 
-import type { ContextModalProps } from "@mantine/modals";
+import type { ServicoForm } from "@/types";
 
 export default function ServicoModalForm({
-  context,
-  id,
-  innerProps,
-}: ContextModalProps<{
-  id: string;
+  empresaId,
+  servico,
+}: {
   empresaId: string;
-}>) {
+  servico?: ServicoForm;
+}) {
+  const { isMutating, setIsFetching } = useFormMutation();
+  const { getInputProps, onSubmit } = useServicoForm(servico);
+
+  const handleSubmit = async (values: ServicoForm) => {
+    setIsFetching(true);
+
+    const response = await createServico(empresaId, values);
+
+    setIsFetching(false);
+    if (response.status !== 200) {
+      errorNotification(response);
+    } else {
+      sucessNotification(response);
+      modals.closeAll();
+    }
+  };
+
   return (
-    <div className="flex flex-col gap-4">
-      <h2>ID: {innerProps.id}</h2>
-      <h2>EmpresaID: {innerProps.empresaId}</h2>
-      <Button onClick={() => context.closeModal(id)}>Close</Button>
-    </div>
+    <form onSubmit={onSubmit(handleSubmit)}>
+      <div className="flex flex-col p-4">
+        <TextInput
+          {...getInputProps("desig_sistema")}
+          label="Designação p/ Sistema"
+        />
+        <TextInput
+          {...getInputProps("desig_ecra")}
+          label="Designação p/ Ecrã"
+        />
+        <TextInput
+          {...getInputProps("desig_tecla_seleccao")}
+          label="Designação p/ Selecção"
+        />
+      </div>
+      <div className="flex gap-2 justify-end bg-modal-footer p-4 border-t border-modal-footer-b">
+        <Button
+          variant="default"
+          onClick={() => {
+            modals.closeAll();
+          }}
+        >
+          Cancelar
+        </Button>
+        <Button type="submit" loading={isMutating}>
+          {servico?.id ? "Editar" : "Criar"} Serviço
+        </Button>
+      </div>
+    </form>
   );
 }
