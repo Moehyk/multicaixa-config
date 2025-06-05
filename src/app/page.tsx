377ccd78f0854ebc435ca8242db0da1861b49d05
Ruben Silva <rubendacostaesilva@gmail.com>
2025-06-05 17:15:33 +0100

    Update component
    - removed dummy data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,3 @@
-import { Empresa } from "@prisma/client";
-
-const empresa: Empresa = {
-  id: "1",
-  utilizadorId: "1",
-  cae: "567",
-  nome: "Empresa",
-  numero_pessoa_colectiva: "null",
-  sigla: "123",
-  morada: "Rua",
-  localidade: "null",
-  responsavel: "Responsavel",
-  telefone: "null",
-  email: "empresa@empresa.com",
-  numero_entidade: "1",
-  desig_ecra: "1234567890",
-  desig_tecla_seleccao: "1234567890",
-};
-
 import { Anchor } from "@mantine/core";
 
 export default function Home() {
