ef39cd323ceddacc28266a6c68c307162d9c5082
Ruben Silva <rubendacostaesilva@gmail.com>
2025-06-11 11:26:46 +0100

    Create component
    - component that will be displayed in case of errors

---
+++ src/components/GridError.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Alert } from "@mantine/core";
+import { IconAlertTriangle } from "@tabler/icons-react";
+
+export default function GridError({ message }: { message: string }) {
+  const icon = <IconAlertTriangle />;
+
+  return (
+    <Alert
+      variant="light"
+      color="red"
+      title={message}
+      icon={icon}
+      w="100%"
+      styles={{
+        root: {
+          border: "1px solid var(--mantine-color-red-light-color)",
+        },
+        wrapper: {
+          alignItems: "center",
+          gap: "0.5rem",
+        },
+        body: {
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        },
+        icon: {
+          width: "2rem",
+          height: "2rem",
+          margin: 0,
+          justifyContent: "center",
+        },
+        title: {
+          fontWeight: 500,
+          fontSize: "1rem",
+        },
+      }}
+    />
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as PageDisplayer } from "./PageDisplayer";
 export { default as Grid } from "./Grid";
 export { default as GridHeader } from "./GridHeader";
+export { default as GridError } from "./GridError";
 export { default as GlobalLoader } from "./GlobalLoader";
 export { default as ErrorWidget } from "./ErrorWidget";
 
