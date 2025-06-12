a6ced8a765ef2e306107d1e5689545eff13e34ef
Ruben Silva <rubendacostaesilva@gmail.com>
2025-06-12 16:10:07 +0100

    Create component
    - component that will be rendered in case of not existing button grid component

---
+++ src/components/multicaixa/NoMcxGrid.tsx
@@ -0,0 +1,15 @@
+import { Paper } from "@mantine/core";
+
+import type { DataModel } from "@/types";
+
+export default function NoMcxGrid({ dataModel }: { dataModel: DataModel }) {
+  const unavailableText: { [key: string]: string } = {
+    servico: "Sem serviços disponíveis",
+    produto: "Sem produtos disponíveis",
+  };
+  return (
+    <Paper p="xl" className="mx-auto mb-80 text-center shadow-none">
+      <p className="text-xl font-medium">{unavailableText[dataModel]}</p>
+    </Paper>
+  );
+}
