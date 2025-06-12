00460aa88a76fed3a2b3f7d33ba5ee7589c18d13
Ruben Silva <rubendacostaesilva@gmail.com>
2025-06-12 16:09:10 +0100

    Create component
    - button component for Multicaixa

---
+++ src/components/multicaixa/McxSelectBtn.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { UnstyledButton } from "@mantine/core";
+
+export default function McxSelectBtn({
+  clickHandler,
+  selectKey,
+  selectName,
+}: {
+  clickHandler: () => void;
+  selectKey: string;
+  selectName: string;
+}) {
+  return (
+    <UnstyledButton
+      className="flex items-center justify-between bg-white h-28 px-4 rounded border-[6px] border-solid border-blue-950 text-2xl font-bold"
+      onClick={clickHandler}
+    >
+      {Number(selectKey) % 2 == 0 && (
+        <>
+          {selectName}
+          <div className="h-16 w-16 bg-orange-400 rounded flex justify-center items-center text-white text-3xl font-semibold">
+            {selectKey}
+          </div>
+        </>
+      )}
+
+      {Number(selectKey) % 2 != 0 && (
+        <>
+          <div className="h-16 w-16 bg-orange-400 rounded flex justify-center items-center text-white text-3xl font-semibold">
+            {selectKey}
+          </div>
+          {selectName}
+        </>
+      )}
+    </UnstyledButton>
+  );
+}
