f63131476efeedf1db4e6a94bcd14244184d98e4
Ruben Silva <rubendacostaesilva@gmail.com>
2025-06-13 07:30:45 +0100

    Create component
    - for closing the modal

---
+++ src/components/multicaixa/McxCloseBtn.tsx
@@ -0,0 +1,14 @@
+import { modals } from "@mantine/modals";
+
+import { Button, ActionIcon } from "@mantine/core";
+import { IconX } from "@tabler/icons-react";
+
+export default function McxCloseBtn() {
+  return (
+    <div className="flex justify-end p-2">
+      <ActionIcon color="orange" onClick={() => modals.closeAll()}>
+        <IconX size={20} />
+      </ActionIcon>
+    </div>
+  );
+}
