911af2d9a8cfbed9885bb71303c46ccde4988bfb
Ruben Silva <rubendacostaesilva@gmail.com>
2025-06-13 13:24:53 +0100

    Create component
    - toolbar component for closing or resetting Multicaixa

---
+++ src/components/multicaixa/McxToolbar.tsx
@@ -0,0 +1,40 @@
+import { modals } from "@mantine/modals";
+import { useMulticaixaController } from "@/context/multicaixa-controller";
+
+import { Button } from "@mantine/core";
+import { IconX } from "@tabler/icons-react";
+
+export default function McxToolbar({ title }: { title: string }) {
+  const { view } = useMulticaixaController();
+
+  return (
+    <div className="flex justify-end p-2">
+      <div className="flex gap-2">
+        {view !== "empresa" && (
+          <Button
+            size="compact-sm"
+            color="cyan"
+            leftSection={<IconX size={16} />}
+            onClick={() =>
+              useMulticaixaController.setState({
+                desigEcra: title,
+                ecraSecondary: "Escolha um serviço",
+                view: "empresa",
+              })
+            }
+          >
+            Reset
+          </Button>
+        )}
+        <Button
+          size="compact-sm"
+          color="orange"
+          leftSection={<IconX size={16} />}
+          onClick={() => modals.closeAll()}
+        >
+          Sair
+        </Button>
+      </div>
+    </div>
+  );
+}
