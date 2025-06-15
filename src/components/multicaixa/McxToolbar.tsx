1123577f3f788dc5c754c0daf8981a820670285b
Ruben Silva <rubendacostaesilva@gmail.com>
2025-06-15 16:13:04 +0100

    Update component
    - setup condition for rendering reset button and navigation

--- src/components/multicaixa/McxToolbar.tsx
+++ src/components/multicaixa/McxToolbar.tsx
@@ -2,25 +2,26 @@
 
 import { useRouter, useParams } from "next/navigation";
 
-import { modals } from "@mantine/modals";
-import { useMulticaixaController } from "@/context/multicaixa-controller";
-
 import { Button } from "@mantine/core";
 import { IconX } from "@tabler/icons-react";
 
-export default function McxToolbar({ title }: { title: string }) {
+export default function McxToolbar() {
   const { push } = useRouter();
+  const params = useParams();
 
   return (
     <div className="flex justify-end p-2">
       <div className="flex gap-2">
-        <Button
-          size="compact-sm"
-          color="cyan"
-          leftSection={<IconX size={16} />}
-        >
-          Reset
-        </Button>
+        {Object.keys(params).length > 1 && (
+          <Button
+            size="compact-sm"
+            color="cyan"
+            leftSection={<IconX size={16} />}
+            onClick={() => push(`/multicaixa/mcx/${params.empresaId}`)}
+          >
+            Reset
+          </Button>
+        )}
         <Button
           size="compact-sm"
           color="orange"
