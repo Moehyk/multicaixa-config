59a4ea5837f9c4ffa5945373a904e9940f1b0e06
Ruben Silva <rubendacostaesilva@gmail.com>
2025-06-18 14:12:40 +0100

    Create component
    - form component for creating "pagamento" data

---
+++ src/components/pagamento/CreatePagamentoForm.tsx
@@ -0,0 +1,10 @@
+import { TextInput } from "@mantine/core";
+
+export default function CreatePagamentoForm() {
+  return (
+    <div className="py-8">
+      <TextInput label="Descrição" placeholder="Descrição do produto" />
+      <TextInput label="Descrição" placeholder="Descrição do produto" />
+    </div>
+  );
+}
---
+++ src/components/pagamento/index.ts
@@ -0,0 +1,1 @@
+export { default as CreatePagamentoForm } from "./CreatePagamentoForm";
