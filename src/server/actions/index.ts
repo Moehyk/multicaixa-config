c499da54386108b916118311a2e1d3c1c8f8e861
Ruben Silva <rubendacostaesilva@gmail.com>
2025-06-18 12:52:30 +0100

    Update services
    - restructure services

--- src/server/actions/index.ts
+++ src/server/actions/index.ts
@@ -3,22 +3,10 @@
 import { api } from "..";
 
 import { Empresa } from "@prisma/client";
-import { CreateServicoParams } from "@/types";
+import { CreateServicoParams, CreateProdutoParams } from "@/types";
 
 export const upsertEmpresa = async (values: Empresa) => {
   const response = await api.empresa.create(values);
-
-  return response;
-};
-
-export const getServicos = async (empresaId: string) => {
-  const response = await api.servico.getAll(empresaId);
-
-  return response;
-};
-
-export const getServico = async (id: string) => {
-  const response = await api.servico.get(id);
 
   return response;
 };
@@ -34,8 +22,49 @@ export const upsertServico = async ({
   }
 };
 
+export const getServico = async (id: string) => {
+  const response = await api.servico.get(id);
+
+  return response;
+};
+
+export const getServicos = async (empresaId: string) => {
+  const response = await api.servico.getAll(empresaId);
+
+  return response;
+};
+
 export const deleteServico = async (id: string) => {
   const response = await api.servico.delete(id);
+
+  return response;
+};
+
+export const upsertProduto = async ({
+  servicoId,
+  input,
+}: CreateProdutoParams) => {
+  if (input.id) {
+    return await api.produto.update(input);
+  } else {
+    return await api.produto.create(servicoId, input);
+  }
+};
+
+export const getProduto = async (id: string) => {
+  const response = await api.produto.get(id);
+
+  return response;
+};
+
+export const getProdutos = async (servicoId: string) => {
+  const response = await api.produto.getAll(servicoId);
+
+  return response;
+};
+
+export const deleteProduto = async (id: string) => {
+  const response = await api.produto.delete(id);
 
   return response;
 };
--- src/server/routes/produto.ts
+++ src/server/routes/produto.ts
@@ -1,105 +1,236 @@
 import { cache } from "react";
 import { revalidatePath } from "next/cache";
 import { db } from "..";
-import { CreateProdutoParams } from "@/types";
+import {
+  validateCuid,
+  validateUser,
+  validateInputs,
+  throwValidationError,
+  processErrors,
+} from "@/utils/errors";
+
+import { ProdutoForm, ServicoForm } from "@/types";
 
 export const produto = {
-  create: async ({ id, input, serviceId }: CreateProdutoParams) => {
+  create: async (servicoId: string, input: ProdutoForm) => {
+    const user = await validateUser();
+    const isCuidValid = validateCuid(servicoId);
+    const { isInputsValid, message } = validateInputs(input);
+
     try {
-      await db.produto.upsert({
-        where: {
-          id: id,
-        },
-        create: {
+      throwValidationError({
+        user,
+        cuid: isCuidValid,
+        data: "servico",
+        inputs: isInputsValid,
+        message,
+      });
+
+      const produto = await db.produto.create({
+        data: {
+          servicoId: servicoId,
           desig_ecra: input.desig_ecra,
           desig_tecla_seleccao: input.desig_tecla_seleccao,
           type: input.type,
-          servicoId: serviceId,
-        },
-        update: {
-          desig_ecra: input.desig_ecra,
-          desig_tecla_seleccao: input.desig_tecla_seleccao,
-          servicoId: serviceId,
         },
       });
 
-      revalidatePath("/multicaixa/entidades/[id]/servicos/[id]", "page");
+      revalidatePath("/multicaixa", "page");
 
-      if (!id) {
-        return {
-          status: 200,
-          message: "Produto criado com sucesso.",
-        };
-      } else {
-        return {
-          status: 200,
-          message: "Produto editado com sucesso.",
-        };
-      }
+      return {
+        status: 200,
+        message: "Produto criado",
+        data: produto,
+      };
     } catch (error) {
-      if (!id) {
-        return {
-          status: 400,
-          message: "Aconteceu um erro ao tentar criar o produto.",
-          error,
-        };
+      if (error instanceof Error) {
+        const response = processErrors(error, {
+          cuid: isCuidValid,
+          inputs: isInputsValid,
+          user: user,
+        });
+
+        return response!;
       } else {
-        return {
-          status: 400,
-          message: "Aconteceu um erro ao tentar editar o produto.",
-          error,
-        };
+        console.error("Unknown Error Type:", error);
       }
+      return {
+        status: 500,
+        message: "Ocorreu um erro ao processar sua solicitação",
+        error,
+      };
+    }
+  },
+
+  update: async (input: ProdutoForm) => {
+    const user = await validateUser();
+    const isCuidValid = validateCuid(input.id);
+    const { isInputsValid, message } = validateInputs(input);
+
+    try {
+      throwValidationError({
+        user,
+        cuid: isCuidValid,
+        data: "produto",
+        inputs: isInputsValid,
+        message,
+      });
+
+      const produto = await db.produto.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          desig_ecra: input.desig_ecra,
+          desig_tecla_seleccao: input.desig_tecla_seleccao,
+        },
+      });
+
+      revalidatePath("/multicaixa", "page");
+
+      return {
+        status: 200,
+        message: "Produto actualizado",
+        data: produto,
+      };
+    } catch (error) {
+      if (error instanceof Error) {
+        const response = processErrors(error, {
+          cuid: isCuidValid,
+          inputs: isInputsValid,
+          user: user,
+        });
+
+        return response!;
+      } else {
+        console.error("Unknown Error Type:", error);
+      }
+      return {
+        status: 500,
+        message: "Ocorreu um erro ao processar sua solicitação",
+        error,
+      };
     }
   },
 
   get: cache(async (id: string) => {
+    const user = await validateUser();
+    const isCuidValid = validateCuid(id);
+
     try {
+      throwValidationError({
+        user,
+        cuid: isCuidValid,
+        data: "produto",
+        inputs: true,
+      });
+
       const produto = await db.produto.findUnique({
         where: {
           id: id,
+        },
+        include: {
+          carregamento: true,
+          pagamento: true,
+          recargas: true,
         },
       });
 
       return { data: produto, status: 200 };
     } catch (error) {
-      return { status: 400, message: "Produto não encontrado.", error };
+      if (error instanceof Error) {
+        const response = processErrors(error, {
+          cuid: isCuidValid,
+          inputs: true,
+          user: user,
+        });
+
+        return response!;
+      } else {
+        console.error("Unknown Error Type:", error);
+      }
+      return {
+        status: 500,
+        message: "Ocorreu um erro ao processar sua solicitação",
+        error,
+      };
     }
   }),
 
   getAll: cache(async (id: string) => {
+    const user = await validateUser();
+    const isCuidValid = validateCuid(id);
+
     try {
-      const data = await db.produto.findMany({
+      throwValidationError({
+        user,
+        cuid: isCuidValid,
+        data: "servico",
+        inputs: true,
+      });
+
+      const produtos = await db.produto.findMany({
         where: {
           servicoId: id,
         },
       });
 
-      return { status: 200, data };
+      return { status: 200, data: produtos };
     } catch (error) {
+      if (error instanceof Error) {
+        const response = processErrors(error, {
+          cuid: isCuidValid,
+          inputs: true,
+          user: user,
+        });
+
+        return response!;
+      } else {
+        console.error("Unknown Error Type:", error);
+      }
       return {
-        status: 400,
-        message: "Lista de produtos não encontrada.",
+        status: 500,
+        message: "Ocorreu um erro ao processar sua solicitação",
         error,
       };
     }
   }),
 
   delete: async (id: string) => {
+    const user = await validateUser();
+    const isCuidValid = validateCuid(id);
     try {
+      throwValidationError({
+        user,
+        cuid: isCuidValid,
+        data: "produto",
+        inputs: true,
+      });
+
       await db.produto.delete({
         where: {
           id: id,
         },
       });
 
-      revalidatePath("/multicaixa/entidades/[id]/servicos/[id]", "page");
+      revalidatePath("/multicaixa", "page");
 
       return { status: 200, message: "Produto apagado com sucesso." };
     } catch (error) {
+      if (error instanceof Error) {
+        const response = processErrors(error, {
+          cuid: isCuidValid,
+          inputs: true,
+          user: user,
+        });
+
+        return response!;
+      } else {
+        console.error("Unknown Error Type:", error);
+      }
       return {
-        status: 400,
-        message: "Aconteceu um erro ao tentar apagar o produto.",
+        status: 500,
+        message: "Ocorreu um erro ao processar sua solicitação",
         error,
       };
     }
