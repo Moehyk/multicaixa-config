11f2ddf0a5b7581bc83b533b011df5133002d4a5
Ruben Silva <rubendacostaesilva@gmail.com>
2025-06-18 14:51:38 +0100

    Update component
    - the first two inputs will now be rendered individually in each form

--- src/components/produto/CriarProdutoForm.tsx
+++ src/components/produto/CriarProdutoForm.tsx
@@ -4,26 +4,21 @@ import { Card, TextInput, Button, Tabs } from "@mantine/core";
 import { CardTitle } from "@/components";
 import { CreatePagamentoForm } from "@/components/pagamento";
 
+import { tabsTabStyles } from "@/constants/styles";
+
 export default function CriarProdutoForm({ id }: { id: string }) {
   return (
     <Card withBorder p={32}>
-      <div className="pb-8">
-        <CardTitle title="Criar Produto" />
-        <div className="flex w-full  gap-4">
-          <TextInput label="Designação p/ ecrã" className="flex-1" />
-          <TextInput
-            label="Designação p/ tecla de selecção"
-            className="flex-1"
-          />
-        </div>
-      </div>
-      <Tabs defaultValue="pagamento" variant="default">
+      <CardTitle title="Criar Produto" />
+      <Tabs defaultValue="pagamento" variant="pills">
         <Tabs.List grow>
-          <Tabs.Tab value="pagamento" className="border-l-0 rounded-s-none">
+          <Tabs.Tab value="pagamento" className={tabsTabStyles}>
             Pagamento
           </Tabs.Tab>
-          <Tabs.Tab value="carregamentos">Carregamentos</Tabs.Tab>
-          <Tabs.Tab value="recargas" className="border-r-0 rounded-e-none">
+          <Tabs.Tab value="carregamentos" className={tabsTabStyles}>
+            Carregamentos
+          </Tabs.Tab>
+          <Tabs.Tab value="recargas" className={tabsTabStyles}>
             Recargas
           </Tabs.Tab>
         </Tabs.List>
