49c7c8bc25c798c13f8a3a6d5cfbc21bd7be1a6b
Ruben Silva <rubendacostaesilva@gmail.com>
2025-06-19 12:34:52 +0100

    Update component
    - removed dummy data rendering

--- src/app/multicaixa/page.tsx
+++ src/app/multicaixa/page.tsx
@@ -1,7 +1,5 @@
 import { getEmpresa } from "@/server/services";
-import { entidade } from "../dummyData";
 
-import { Grid, GridHeader } from "@/components";
 import { NoEmpresa, Empresa } from "@/components/empresa";
 
 export default async function MulticaixaPage() {
@@ -11,27 +9,7 @@ export default async function MulticaixaPage() {
     return <NoEmpresa />;
   }
 
-  return (
-    <>
-      <div className="mb-16">
-        <GridHeader empresaId={empresa.id} />
-        <Grid>
-          {entidade.servicos.length === 0 && <Grid.NoServico />}
-          {entidade.servicos.length > 0 &&
-            entidade.servicos.map((servico) => (
-              <Grid.Servico key={servico.id} servico={servico}>
-                {servico.produtos.length === 0 && <Grid.NoProduto />}
-                {servico.produtos.length > 0 &&
-                  servico.produtos.map((produto) => (
-                    <Grid.Produto {...produto} key={produto.id} />
-                  ))}
-              </Grid.Servico>
-            ))}
-        </Grid>
-      </div>
-      <Empresa {...empresa} />
-    </>
-  );
+  return <Empresa {...empresa} />;
 }
 
 export const dynamic = "force-dynamic";
