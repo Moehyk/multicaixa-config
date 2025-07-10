ee14da2d77736af74017ef5d0d410cac46ad4a33
Ruben Silva <rubendacostaesilva@gmail.com>
2025-07-10 10:29:39 +0100

    Update component styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,15 +8,9 @@ import Image from "next/image";
 import logo_light from "@/assets/logo_light.svg";
 import logo_dark from "@/assets/logo_dark.svg";
 
-function StickyHeader() {
-  return (
-    <header className="sticky top-0 left-0 right-0 z-50 p-4">Header</header>
-  );
-}
-
 export default function Header() {
   return (
-    <header className="fixed inset-x-0 top-0 z-50 w-full flex items-center h-20 bg-header border-b border-header-border backdrop-blur-lg">
+    <header className="fixed inset-x-0 top-0 z-50 w-full flex items-center h-20 bg-header/75 border-b border-header-border backdrop-blur-md">
       <div className="container w-full flex items-center justify-between">
         <Image
           src={logo_light}
