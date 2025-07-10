cf409283089cace15921ef51bed9d7fd74ab4603
Ruben Silva <rubendacostaesilva@gmail.com>
2025-07-10 14:56:48 +0100

    Update exports

--- src/components/produto/index.ts
+++ src/components/produto/index.ts
@@ -1,2 +1,5 @@
 export { default as CriarProdutoForm } from "./CriarProdutoForm";
+export { default as UpdatePagamento } from "./UpdatePagamento";
+export { default as CreateCarregamentoForm } from "./CreateCarregamentoForm";
+export { default as CreateRecargasForm } from "./CreateRecargasForm";
 export { default as GridProduto } from "./GridProduto";
