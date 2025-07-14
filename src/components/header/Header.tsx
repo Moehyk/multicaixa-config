663fccb4da2ba57048f5c4d0901c8dbc205d34e8
Ruben Silva <rubendacostaesilva@gmail.com>
2025-07-14 15:12:50 +0100

    Update  component
    - a complete restructure of User component, it will conditionally render loading component while loading state, login button if user not authenticated, user info if user is authenticated, and depending on page, enter or logout buttons
    
    - Header component height increased

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,10 @@
 "use client";
-import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+
 import { useColorScheme } from "@mantine/hooks";
 
 import Link from "next/link";
 import User from "./User";
-import { Anchor, Button } from "@mantine/core";
-import { IconDeviceDesktop } from "@tabler/icons-react";
+import { Anchor } from "@mantine/core";
 
 import Image from "next/image";
 
@@ -13,10 +12,8 @@ import logo_light from "@/assets/logo_light.svg";
 import logo_dark from "@/assets/logo_dark.svg";
 
 export default function Header() {
-  const { isAuthenticated } = useKindeBrowserClient();
-
   return (
-    <header className="fixed inset-x-0 top-0 z-50 w-full flex items-center h-20 bg-header/75 border-b border-header-border backdrop-blur-md">
+    <header className="fixed inset-x-0 top-0 z-50 w-full flex items-center h-24 bg-header/75 border-b border-header-border backdrop-blur-md">
       <div className="container w-full flex items-center justify-between">
         <Image
           src={logo_light}
@@ -25,25 +22,7 @@ export default function Header() {
           height={40}
           className="object-contain"
         />
-        <div>
-          {!isAuthenticated && (
-            <Button component={Link} href="/multicaixa">
-              Entrar
-            </Button>
-          )}
-          {isAuthenticated && (
-            <div className="flex gap-2">
-              <Button
-                leftSection={<IconDeviceDesktop size={20} stroke={1.75} />}
-                component={Link}
-                href="/multicaixa/mcx"
-              >
-                Multicaixa
-              </Button>
-              <User />
-            </div>
-          )}
-        </div>
+        <User />
       </div>
     </header>
   );
--- src/components/header/User.tsx
+++ src/components/header/User.tsx
@@ -1,40 +1,83 @@
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { usePathname } from "next/navigation";
 
 import Link from "next/link";
-import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
-import { Menu, Button } from "@mantine/core";
-import { IconBuilding, IconDoorExit, IconSelector } from "@tabler/icons-react";
+import { LogoutLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { Button, Avatar, Loader } from "@mantine/core";
+import { IconLogin, IconLogout } from "@tabler/icons-react";
+
+function UserWrapper({ children }: React.PropsWithChildren) {
+  return (
+    <div className="flex items-center gap-8 border border-border rounded-full p-2 ">
+      {children}
+    </div>
+  );
+}
+
+function LoginButton() {
+  return (
+    <Button
+      component={Link}
+      href="/multicaixa"
+      radius={999}
+      rightSection={<IconLogin size={20} />}
+    >
+      Entrar
+    </Button>
+  );
+}
 
 export default function User() {
-  const { user, isLoading } = useKindeBrowserClient();
+  const { user, isLoading, isAuthenticated } = useKindeBrowserClient();
+  const isHome = usePathname() === "/";
+
+  if (isLoading) {
+    return (
+      <UserWrapper>
+        <div className="flex items-center h-[36px]">
+          <Loader size="md" type="dots" w={200} />
+        </div>
+      </UserWrapper>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return (
+      <Button
+        component={LoginLink}
+        radius={999}
+        rightSection={<IconLogin size={20} />}
+      >
+        Entrar
+      </Button>
+    );
+  }
 
   return (
-    <Menu>
-      <Menu.Target>
+    <UserWrapper>
+      <div className="flex items-center gap-2">
+        <Avatar
+          src={user?.picture}
+          alt="Avatar"
+          radius="xl"
+          className="border border-border"
+        />
+        <span>
+          <span>Bem vindo&#44;&nbsp;</span>
+          <span className="font-semibold">{`${user?.given_name} ${user?.family_name}`}</span>
+        </span>
+      </div>
+      {isHome && <LoginButton />}
+      {!isHome && (
         <Button
-          loading={isLoading!}
-          variant="outline"
-          rightSection={<IconSelector size={20} stroke={1.75} />}
-        >
-          {`${user?.given_name} ${user?.family_name}`}
-        </Button>
-      </Menu.Target>
-      <Menu.Dropdown>
-        <Menu.Item
-          component={Link}
-          href="/multicaixa/empresa"
-          leftSection={<IconBuilding size={14} />}
-        >
-          Editar Empresa
-        </Menu.Item>
-        <Menu.Item
           component={LogoutLink}
           color="red"
-          leftSection={<IconDoorExit size={14} />}
+          radius={999}
+          rightSection={<IconLogout size={20} />}
         >
           Sair
-        </Menu.Item>
-      </Menu.Dropdown>
-    </Menu>
+        </Button>
+      )}
+    </UserWrapper>
   );
 }
