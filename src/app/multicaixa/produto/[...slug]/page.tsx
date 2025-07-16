b17447f34ec3fd5519821e4d1c717e712b780e11
Ruben Silva <rubendacostaesilva@gmail.com>
2025-07-16 15:56:44 +0100

    Update component
    - implemented condition for rendering ProdutoPagamento form

--- src/app/multicaixa/produto/[...slug]/page.tsx
+++ src/app/multicaixa/produto/[...slug]/page.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import { getProduto } from "@/server/services";
+import { notFound } from "next/navigation";
+
+import { UpdatePagamento } from "@/components/produto";
 
 export default async function DynamicProdutoPage({
   params,
@@ -7,5 +10,15 @@ export default async function DynamicProdutoPage({
 }) {
   const { slug } = await params;
 
-  return <div>Editar produto {slug[0]}</div>;
+  const { data } = await getProduto(slug[0]);
+
+  if (!data) {
+    notFound();
+  }
+
+  if (data.type === "pagamento" && data.pagamento) {
+    const { pagamento } = data;
+
+    return <UpdatePagamento produto={data} pagamento={pagamento} />;
+  }
 }
