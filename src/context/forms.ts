b22413fbf5c20c8f405015bde210f5ddab42f654
Ruben Silva <rubendacostaesilva@gmail.com>
2025-07-17 11:34:03 +0100

    Create context
    - context for RacargasForm and CarregamentoForm

--- src/context/forms.ts
+++ src/context/forms.ts
@@ -1,6 +1,19 @@
 import { createFormContext } from "@mantine/form";
 
-import type { ProdutoPagamentoForm } from "@/types";
+import type {
+  ProdutoPagamentoForm,
+  ProdutoRecargasForm,
+  ProdutoCarregamentoForm,
+} from "@/types";
 
 export const [PagamentoFormProvider, usePagamentoFormContext, usePagForm] =
   createFormContext<ProdutoPagamentoForm>();
+
+export const [RecargasFormProvider, useRecargasFormContext, useRecaForm] =
+  createFormContext<ProdutoRecargasForm>();
+
+export const [
+  CarregamentoFormProvider,
+  useCarregamentoFormContext,
+  useCarrForm,
+] = createFormContext<ProdutoCarregamentoForm>();
