fbcbd6f6ea997a0e19729ef60d67b5feaa2f2fec
Ruben Silva <rubendacostaesilva@gmail.com>
2025-07-23 13:05:13 +0100

    Delete GlobalLoader component
    - no need to have it on a different type if it's not being used on other places

--- src/app/multicaixa/(main)/loading.tsx
+++ src/app/multicaixa/(main)/loading.tsx
@@ -1,7 +1,23 @@
 "use client";
 
-import { GlobalLoader } from "@/components";
+import { LoadingOverlay, Portal, Loader } from "@mantine/core";
 
 export default function MulticaixaPageLoading() {
-  return <GlobalLoader />;
+  return (
+    <Portal>
+      <LoadingOverlay
+        visible
+        overlayProps={{
+          blur: 2,
+        }}
+        loaderProps={{
+          children: (
+            <div className="bg-body rounded-md p-4 drop-shadow-xl border border-border/50">
+              <Loader size="lg" type="bars" />
+            </div>
+          ),
+        }}
+      />
+    </Portal>
+  );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,7 +4,6 @@ export { default as Grid } from "./Grid";
 export { default as GridHeader } from "./GridHeader";
 export { default as GridItem } from "./GridItem";
 export { default as GridError } from "./GridError";
-export { default as GlobalLoader } from "./GlobalLoader";
 export { default as ErrorWidget } from "./ErrorWidget";
 export { default as GridLoading } from "./GridLoading";
 
