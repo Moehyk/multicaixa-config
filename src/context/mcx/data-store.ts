844da2ac2ec03ba03d17e6b0e501c24c20bb6660
Ruben Silva <rubendacostaesilva@gmail.com>
2025-08-01 11:25:53 +0100

    Update context stores
    - updated empresaStore with a getServico method
    
    - created servicoStore

--- src/context/mcx/data-store.ts
+++ src/context/mcx/data-store.ts
@@ -1,8 +1,23 @@
 import { create } from "zustand";
 
-import type { EmpresaMcxStore } from "@/types";
+import type { EmpresaMcxStore, ServicoStore } from "@/types";
 
-export const empresaStore = create<EmpresaMcxStore>((set) => ({
+export const useEmpresaStore = create<EmpresaMcxStore>((set, get) => ({
   desigEcra: "",
   servicos: [],
+  getServico: (id?: string) => {
+    const servico = get().servicos.find((s) => s.id === id);
+    return servico;
+  },
+}));
+
+export const useServicoStore = create<ServicoStore>((set, get) => ({
+  id: "",
+  desigEcra: "",
+  produtos: [],
+  setProdutos: (id: string) => {
+    const produtos = get().produtos;
+    set({ produtos: produtos.filter((p) => p.id === id) });
+  },
+  setId: (id?: string) => set({ id }),
 }));
