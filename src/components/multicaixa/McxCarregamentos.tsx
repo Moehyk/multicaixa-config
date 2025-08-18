bf8d4ae018144b9f7d5be2c0d9b81cec4f613f2e
Ruben Silva <rubendacostaesilva@gmail.com>
2025-08-18 15:39:59 +0100

    Update component
    - creating a render function that will render depending on montanteTipo

--- src/components/multicaixa/McxCarregamentos.tsx
+++ src/components/multicaixa/McxCarregamentos.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 
 import type { CarregamentoData } from "@/types";
+import type { MontanteTipo } from "@prisma/client";
+
+const renderCarregamento = (montanteTipo: MontanteTipo) => {
+  switch (montanteTipo) {
+    case "montante_pre_definido": {
+      return <div>Carregamento Multi</div>;
+    }
+    case "montante_livre": {
+      return <div>Carregamento Livre</div>;
+    }
+    case "ambos": {
+      return <div>Carregamento Ambos</div>;
+    }
+    default:
+      const _exhaustiveCheck: never = montanteTipo;
+      return null;
+  }
+};
 
 export default function McxCarregamentos({
   desigReferencia,
+  montanteTipo,
 }: NonNullable<CarregamentoData>) {
-  return <div>{desigReferencia}</div>;
+  return <div>{renderCarregamento(montanteTipo)}</div>;
 }
