ea0a0e17a76f1bde7e8c936effda5934da7c460e
Ruben Silva <rubendacostaesilva@gmail.com>
2025-09-02 17:41:18 +0100

    Delete unused components

--- src/components/multicaixa/McxCarregamentoFree.tsx
+++
@@ -1,7 +0,0 @@
-import McxReferenciaMontanteView from "./McxReferenciaMontanteView";
-
-import type { CarregamentoFree } from "@/types";
-
-export default function McxCarregamentoFree(props: CarregamentoFree) {
-  return <McxReferenciaMontanteView {...props} />;
-}
