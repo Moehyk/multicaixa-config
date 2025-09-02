5d2418e88c03409e1ba7f9790d1fedd6e6c0ef3b
Ruben Silva <rubendacostaesilva@gmail.com>
2025-09-02 11:44:43 +0100

    Update component
    - reestructuring our component, no render fucntion for now, because we'll need to pass different onContinue functions

--- src/components/multicaixa/McxCarregamentos.tsx
+++ src/components/multicaixa/McxCarregamentos.tsx
@@ -1,29 +1,86 @@
-import { sortDataArray } from "@/utils/sort-data-array";
+"use client";
 
+import { useState } from "react";
+import { createGridButtons } from "@/utils/create-grid-buttons";
+
+import McxInputsView from "./McxInputsView";
 import McxSelectionView from "./McxSelectionView";
-import McxCarregamentoPre from "./McxCarregamentoPre";
-import McxCarregamentoFree from "./McxCarregamentoFree";
-import McxCarregamentoBoth from "./McxCarregamentoBoth";
+import McxInput from "./McxInput";
+import McxReferenciaMontanteView from "./McxReferenciaMontanteView";
 
-import type { CarregamentoData, RenderCarregamento, GridButton } from "@/types";
+import type { CarregamentoData, McxScreensType, GridButton } from "@/types";
 
-const renderCarregamento = (c: RenderCarregamento) => {
-  switch (c.montanteTipo) {
-    case "montante_pre_definido": {
-      return <McxCarregamentoPre {...c} />;
+function CarregamentoMontantes({
+  montanteTipo,
+  montantes,
+  ...props
+}: NonNullable<CarregamentoData>) {
+  const [screen, setScreen] = useState<McxScreensType>(1);
+
+  const buttons: GridButton[] = createGridButtons(montantes);
+
+  const nextScreen = (screen: McxScreensType): McxScreensType => {
+    if (screen < 3) {
+      return (screen + 1) as McxScreensType;
+    } else {
+      return 3;
     }
-    case "montante_livre": {
-      return <McxCarregamentoFree {...c} />;
-    }
-    case "ambos": {
-      return <McxCarregamentoBoth {...c} />;
-    }
-    default:
-      const _exhaustiveCheck: never = c;
-      return null;
+  };
+
+  return (
+    <>
+      {screen === 1 && (
+        <McxInputsView
+          onCancel={() => setScreen(1)}
+          onContinue={() => setScreen(nextScreen)}
+        >
+          <McxInput
+            valueType="REFERENCIA"
+            tamanhoReferencia={props.tamanhoReferencia}
+            desigReferencia={props.desigReferencia}
+            textoEcraReferencia={props.textoEcraReferencia}
+          />
+        </McxInputsView>
+      )}
+      {screen === 2 && (
+        <McxSelectionView
+          buttons={buttons}
+          target="end"
+          isDefault
+          hasFreeAmount={montanteTipo === "ambos"}
+          toFreeAmount={() => setScreen(3)}
+        />
+      )}
+      {screen === 3 && (
+        <McxInputsView
+          onCancel={() => setScreen(1)}
+          onContinue={() => setScreen(nextScreen)}
+        >
+          <McxInput
+            valueType="MONTANTE"
+            min={props.montanteMin!}
+            max={props.montanteMax!}
+          />
+        </McxInputsView>
+      )}
+    </>
+  );
+}
+
+export default function McxCarregamentos({
+  montanteTipo,
+  montantes,
+  ...props
+}: NonNullable<CarregamentoData>) {
+  if (montanteTipo === "montante_livre") {
+    return <McxReferenciaMontanteView {...props} />;
+  } else {
+    return (
+      <CarregamentoMontantes
+        {...props}
+        montantes={montantes}
+        montanteTipo={montanteTipo}
+      />
+    );
   }
-};
-
-export default function McxCarregamentos(props: NonNullable<CarregamentoData>) {
-  return <>{renderCarregamento(props as RenderCarregamento)}</>;
 }
