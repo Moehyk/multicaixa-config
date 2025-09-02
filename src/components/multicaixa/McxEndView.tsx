57953bf15391171ad7f71307374a4f4cb2662abb
Ruben Silva <rubendacostaesilva@gmail.com>
2025-09-02 13:44:45 +0100

    Update component
    - setup JSX

--- src/components/multicaixa/McxEndView.tsx
+++ src/components/multicaixa/McxEndView.tsx
@@ -1,5 +1,42 @@
 import React from "react";
 
+import McxScreenText from "./McxScreenText";
+import McxSelectBtn from "./McxSelectBtn";
+
+function DadosConfirmaos({ data, title }: { data: string; title: string }) {
+  return (
+    <p className="flex justify-between font-medium">
+      <span>{`${title}:`}</span>
+      <span>{data}</span>
+    </p>
+  );
+}
+
 export default function McxEndView() {
-  return <div>Mcx End View</div>;
+  return (
+    <>
+      <McxScreenText subtext="O nome do produto" />
+      <div className="flex flex-col w-full h-full items-center justify-between px-16 pb-8 pt-16">
+        <div className="bg-brand-900 rounded-lg p-16 flex gap-4 flex-col text-white text-center">
+          <p className="text-xl font-semibold mb-4">
+            Confirme os dados do Serviço:
+          </p>
+          <DadosConfirmaos data="123456789012345" title="Referência" />
+          <DadosConfirmaos data="9,999,999.99" title="Montante" />
+        </div>
+        <div className="w-full flex gap-8 justify-between">
+          <McxSelectBtn
+            clickHandler={() => {}}
+            selectText="Cancelar"
+            selectKey="1"
+          />
+          <McxSelectBtn
+            clickHandler={() => {}}
+            selectText="Confirmar"
+            selectKey="2"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
