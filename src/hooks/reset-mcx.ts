3324bc6d725e118ebafcd8d3e9f291138f2b0482
Ruben Silva <rubendacostaesilva@gmail.com>
2025-09-22 10:38:31 +0100

    Update hook
    - reset hook will also reset the input errrors store

--- src/hooks/reset-mcx.ts
+++ src/hooks/reset-mcx.ts
@@ -1,11 +1,17 @@
 import { modals } from "@mantine/modals";
 import { useViewsStore, useEndViewStore } from "@/context/mcx";
+import {
+  setInputMontanteError,
+  setInputReferenciaError,
+} from "@/context/mcx/input-errors";
 
 export const useResetMcx = () => {
   const { setView } = useViewsStore();
   const { resetEndStore } = useEndViewStore();
 
   return () => {
+    setInputMontanteError(false);
+    setInputReferenciaError(false);
     resetEndStore();
     setView("empresa");
   };
