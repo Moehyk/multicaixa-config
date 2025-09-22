ed986c280ae22039bfa30e5fe9fddb7d37562034
Ruben Silva <rubendacostaesilva@gmail.com>
2025-09-22 10:52:16 +0100

    Update hooks
    - created useReferenciaError and useMontanteError for validating the inputs and in case the validation fails, error state will be updated
    
    - created useContinueWithValidation for handling screen navigation, with input valdiation, and resetting the input focus if validation is failed
    
    - updated useMcxInputActions, that will be the hook used in the components, that will the state for setting the errors state and return the navigation functions and screen state for rendering the screens

--- src/hooks/mcx-inputs-view-actions.ts
+++ src/hooks/mcx-inputs-view-actions.ts
@@ -1,9 +1,46 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useViewsStore, useEndViewStore } from "@/context/mcx";
+import {
+  setInputMontanteError,
+  setInputReferenciaError,
+  useInputRefsStore,
+} from "@/context/mcx/input-errors";
 
-import type { McxScreensType } from "@/types";
+import type { McxScreensType, McxInputActions } from "@/types";
+
+const useReferenciaError = (length: number) => {
+  const [error, setError] = useState<boolean>(false);
+  const { referencia } = useEndViewStore();
+
+  useEffect(() => {
+    if (referencia?.length !== length) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+  }, [referencia]);
+
+  return error;
+};
+
+const useMontanteError = (mmin: number, mmax: number) => {
+  const [error, setError] = useState<boolean>(false);
+  const { montante } = useEndViewStore();
+
+  const monatanteNum = Number(montante.slice(0, -2));
+
+  useEffect(() => {
+    if (monatanteNum < mmin || monatanteNum > mmax) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+  }, [montante]);
+
+  return error;
+};
 
 const useClearMcxInputs = (screen: McxScreensType) => {
   const { setMontante, setReferencia } = useEndViewStore();
@@ -11,26 +48,55 @@ const useClearMcxInputs = (screen: McxScreensType) => {
   return () => (screen === 1 ? setReferencia("") : setMontante(""));
 };
 
-export const useReferenciaMontanteViewActions = () => {
+const useContinueWithValidation = (refErr: boolean, montErr: boolean) => {
   const { setView } = useViewsStore();
+  const { inputRefs } = useInputRefsStore();
   const [screen, setScreen] = useState<McxScreensType>(1);
 
-  const clearHandler = useClearMcxInputs(screen);
-  const continueHandler = () => (screen === 1 ? setScreen(2) : setView("end"));
+  const onContinue = () => {
+    if (screen === 1) {
+      if (refErr) {
+        setInputReferenciaError(true);
+        setTimeout(() => {
+          inputRefs?.current[length - 1]?.focus();
+        }, 0);
+        return;
+      } else {
+        setInputReferenciaError(false);
+      }
+      setScreen(2);
+    } else {
+      if (montErr) {
+        setInputMontanteError(true);
+        setTimeout(() => {
+          inputRefs?.current[9]?.focus();
+        }, 0);
+        return;
+      } else {
+        setInputMontanteError(false);
+      }
+      setView("end");
+    }
+  };
 
-  return { screen, continueHandler, clearHandler };
+  return [[screen, setScreen], onContinue] as const;
 };
 
-export const useCarregamentoMontanteViewActions = () => {
-  const { setView } = useViewsStore();
-  const [screen, setScreen] = useState<McxScreensType>(1);
+export const useMcxInputActions: McxInputActions = (l, min, max) => {
+  const referenciaError = useReferenciaError(l);
+  const montanteError = useMontanteError(min, max);
+
+  const [[screen, setScreen], continueHandler] = useContinueWithValidation(
+    referenciaError,
+    montanteError
+  );
 
   const clearHandler = useClearMcxInputs(screen);
 
   return {
     screen,
-    setScreen,
-    setView,
+    continueHandler,
     clearHandler,
+    setScreen,
   };
 };
