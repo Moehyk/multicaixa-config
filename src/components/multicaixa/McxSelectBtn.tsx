d5a886cdb1288e1a83cabf2f7e663122df01068f
Ruben Silva <rubendacostaesilva@gmail.com>
2025-09-24 16:47:21 +0100

    Update component
    - adding a little motion

--- src/components/multicaixa/McxSelectBtn.tsx
+++ src/components/multicaixa/McxSelectBtn.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import { motion } from "motion/react";
 
 import { UnstyledButton } from "@mantine/core";
 
@@ -13,34 +15,42 @@ export default function McxSelectBtn({
   selectText: string;
   selectSecondarytext?: string;
 }) {
-  return (
-    <UnstyledButton
-      className="flex items-center justify-between bg-white h-28 px-4 rounded border-[6px] border-solid border-blue-950 text-2xl font-bold flex-1"
-      onClick={onClick}
-    >
-      {Number(selectKey) % 2 == 0 && (
-        <>
-          <div>
-            <p> {selectText}</p>
-            <p> {selectSecondarytext}</p>
-          </div>
-          <div className="h-16 w-16 bg-orange-400 rounded flex justify-center items-center text-white text-3xl font-semibold">
-            {selectKey}
-          </div>
-        </>
-      )}
+  const isOdd = Number(selectKey) % 2 != 0;
 
-      {Number(selectKey) % 2 != 0 && (
-        <>
-          <div className="h-16 w-16 bg-orange-400 rounded flex justify-center items-center text-white text-3xl font-semibold">
-            {selectKey}
-          </div>
-          <div className="text-end">
-            <p> {selectText}</p>
-            <p> {selectSecondarytext}</p>
-          </div>
-        </>
-      )}
-    </UnstyledButton>
+  return (
+    <motion.div
+      initial={{ x: isOdd ? -100 : 100, opacity: 0 }}
+      animate={{ x: isOdd ? 0 : 0, opacity: 1 }}
+      transition={{ duration: 0.25 }}
+    >
+      <UnstyledButton
+        className="flex items-center justify-between bg-white h-28 px-4 rounded border-[6px] border-solid border-blue-950 text-2xl font-bold w-full"
+        onClick={onClick}
+      >
+        {!isOdd && (
+          <>
+            <div>
+              <p> {selectText}</p>
+              <p> {selectSecondarytext}</p>
+            </div>
+            <div className="h-16 w-16 bg-orange-400 rounded flex justify-center items-center text-white text-3xl font-semibold">
+              {selectKey}
+            </div>
+          </>
+        )}
+
+        {isOdd && (
+          <>
+            <div className="h-16 w-16 bg-orange-400 rounded flex justify-center items-center text-white text-3xl font-semibold">
+              {selectKey}
+            </div>
+            <div className="text-end">
+              <p> {selectText}</p>
+              <p> {selectSecondarytext}</p>
+            </div>
+          </>
+        )}
+      </UnstyledButton>
+    </motion.div>
   );
 }
