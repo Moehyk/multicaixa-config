5b984551e25c4bbb4f3afcd27018d21e699feb0f
Ruben Silva <rubendacostaesilva@gmail.com>
2025-09-26 14:57:59 +0100

    Update hook
    - adding keys press event

--- src/hooks/mcx-end-view.ts
+++ src/hooks/mcx-end-view.ts
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect } from "react";
 import { useMcxData } from "./useMcxData";
 import { useResetMcx, useCloseMcxModal } from "./reset-mcx";
 import { useEndViewStore } from "@/context/mcx";
@@ -7,6 +10,25 @@ export const useMcxEndView = () => {
   const { montante, referencia, unidades } = useEndViewStore();
   const reset = useResetMcx();
   const closeModal = useCloseMcxModal();
+
+  useEffect(() => {
+    const keyPressHandler = (e: globalThis.KeyboardEvent) => {
+      const key = e.key;
+
+      if (key === "1") {
+        reset();
+      }
+      if (key === "2") {
+        closeModal();
+      }
+    };
+
+    document.addEventListener("keydown", keyPressHandler);
+
+    return () => {
+      document.removeEventListener("keydown", keyPressHandler);
+    };
+  }, [reset, closeModal]);
 
   return {
     produto,
