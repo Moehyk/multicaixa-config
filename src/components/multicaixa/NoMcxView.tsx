c15108f6835f55959e499420c2a531c7c00f0f9b
Ruben Silva <rubendacostaesilva@gmail.com>
2025-09-26 16:17:22 +0100

    Update component
    - changed prop for receiving the view from parent component, and added useNoMcxView hook for accessing the text and back handler function

--- src/components/multicaixa/NoMcxView.tsx
+++ src/components/multicaixa/NoMcxView.tsx
@@ -1,11 +1,20 @@
-import { Paper } from "@mantine/core";
-import McxContentWrapper from "./McxContentWrapper";
+import { useNoMcxView } from "@/hooks/no-mcx-view";
 
-export default function NoMcxView({ text }: { text: string }) {
+import McxContentWrapper from "./McxContentWrapper";
+import { Paper, Button } from "@mantine/core";
+
+import type { Views } from "@/types";
+
+export default function NoMcxView({ view }: { view: Views }) {
+  const [text, back] = useNoMcxView(view);
+
   return (
     <McxContentWrapper>
       <Paper p="xl" className="mx-auto mb-80 text-center shadow-none">
-        <p className="text-xl font-medium">{text}</p>
+        <p className="text-xl font-medium mb-8">{text}</p>
+        <Button color="orange" onClick={back}>
+          Voltar
+        </Button>
       </Paper>
     </McxContentWrapper>
   );
