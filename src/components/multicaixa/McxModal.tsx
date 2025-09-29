78f74fba16f59ca71b32e3cdc304e498a2873fe4
Ruben Silva <rubendacostaesilva@gmail.com>
2025-09-29 11:44:56 +0100

    Update component
    - created individual components for rendering McxApp and McxAppPreview
    
    - created a render function for rendering the app from the type prop

--- src/components/multicaixa/McxModal.tsx
+++ src/components/multicaixa/McxModal.tsx
@@ -4,14 +4,39 @@ import { useViewsStore } from "@/context/mcx";
 
 import McxToolbar from "./McxToolbar";
 
-export default function McxModal() {
-  const { McxView } = useViewsStore();
+import type { McxAppType } from "@/types";
+import type { ContextModalProps } from "@mantine/modals";
 
+function McxApp() {
+  const { McxView } = useViewsStore();
+  return <McxView />;
+}
+
+function McxAppPreview() {
+  return <div>Preview</div>;
+}
+
+const renderApp = (type: McxAppType) => {
+  switch (type) {
+    case "APP": {
+      return <McxApp />;
+    }
+    case "PREVIEW": {
+      return <McxAppPreview />;
+    }
+  }
+};
+
+export default function McxModal({
+  innerProps: { type },
+}: ContextModalProps<{
+  type: McxAppType;
+}>) {
   return (
     <div className="bg-brand-700 h-[820px]">
       <div className="h-full flex flex-col">
         <McxToolbar />
-        <McxView />
+        {renderApp(type)}
       </div>
     </div>
   );
