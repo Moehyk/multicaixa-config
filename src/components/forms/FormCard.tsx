412bfe7a3fb23a11047894ab58df5294378e5c5c
Ruben Silva <rubendacostaesilva@gmail.com>
2025-09-29 11:58:38 +0100

    Update component
    - added button component for opening the preview modal

--- src/components/forms/FormCard.tsx
+++ src/components/forms/FormCard.tsx
@@ -1,4 +1,9 @@
-import { Card, Divider } from "@mantine/core";
+"use client";
+
+import { openContextModal } from "@mantine/modals";
+
+import { Card, Divider, Button } from "@mantine/core";
+import { IconDeviceDesktop } from "@tabler/icons-react";
 
 export default function FormCard({
   children,
@@ -11,10 +16,27 @@ export default function FormCard({
 }) {
   return (
     <Card withBorder p={32}>
-      <div className="mb-10 flex items-center gap-4">
-        <span className="text-2xl font-semibold">{header}</span>
-        <Divider orientation="vertical" />
-        <span className="text-lg font-medium">{subheader}</span>
+      <div className="mb-16 flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <span className="text-2xl font-semibold">{header}</span>
+          <Divider orientation="vertical" />
+          <span className="text-lg font-medium">{subheader}</span>
+        </div>
+        <Button
+          variant="outline"
+          rightSection={<IconDeviceDesktop size={20} />}
+          onClick={() =>
+            openContextModal({
+              modal: "mcx-modal",
+              size: 1200,
+              innerProps: {
+                type: "PREVIEW",
+              },
+            })
+          }
+        >
+          Visualizar
+        </Button>
       </div>
       {children}
     </Card>
