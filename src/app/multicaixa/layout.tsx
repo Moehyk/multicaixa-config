1671fcb334299c3a515f16eadcda2d48e6e1d2c2
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-02 15:49:17 +0100

    Update component
    - we're not doing parallel routing to intercept routes anymore

--- src/app/multicaixa/layout.tsx
+++ src/app/multicaixa/layout.tsx
@@ -2,16 +2,13 @@ import { EmpresaToolbar } from "@/components";
 
 export default function MulticaixaLayout({
   children,
-  mcxmodal,
 }: {
   children: React.ReactNode;
-  mcxmodal: React.ReactNode;
 }) {
   return (
     <>
       <EmpresaToolbar />
       <div className="mt-28">{children}</div>
-      {mcxmodal}
     </>
   );
 }
