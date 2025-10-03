8686949b0515e502d062d41eca0786b8aec142f5
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-03 10:46:26 +0100

    Delete unused hook
    - we've realocated it to the forms folder

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,4 +1,3 @@
-export { useFormMutation } from "./useFormMutation";
 export { useMotionIcon } from "./useMotionIcon";
 export { useInputSeparators } from "./useInputSeparators";
 export { useMcxCustomInput } from "./useMcxCustomInput";
