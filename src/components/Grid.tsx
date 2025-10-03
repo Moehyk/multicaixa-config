a42395fa9cd1d6b0bc87e5bada85933d68e18091
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-03 10:53:18 +0100

    Update component
    - reorder some stuff

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -5,7 +5,7 @@ export default function Grid({ children }: { children: React.ReactNode }) {
   return <div className="flex flex-col gap-4">{children}</div>;
 }
 
-Grid.Produtos = GridProdutos;
-Grid.NoProduto = GridNoProduto;
 Grid.Servicos = GridServicos;
 Grid.NoServico = GridNoServico;
+Grid.Produtos = GridProdutos;
+Grid.NoProduto = GridNoProduto;
