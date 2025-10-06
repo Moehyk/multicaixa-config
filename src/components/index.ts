665b79f3b82f4efc86e0a44c88fd9e7b9edb568b
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-06 10:20:31 +0100

    Update imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,8 @@ export { default as GridHeader } from "./GridHeader";
 export { default as GridItem } from "./GridItem";
 export { default as GridError } from "./GridError";
 export { default as ErrorWidget } from "./ErrorWidget";
+export { PageWrapper } from "./wrappers";
+export { Header } from "./header";
+export { Footer } from "./footer";
 export { EmpresaLoader, NoEmpresa, EmpresaToolbar } from "./empresa";
 export { GridServicos, ServicosList } from "./servico";
