c401126436e06ee2d5410ac3814b416c72920dbe
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-06 10:22:01 +0100

    Update styling

--- src/components/wrappers/PageWrapper.tsx
+++ src/components/wrappers/PageWrapper.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function PageWrapper({ children }: React.PropsWithChildren) {
   return (
-    <main className="flex-1 mt-24">
+    <main className="flex-1">
       <div className="container flex flex-col">{children}</div>
     </main>
   );
