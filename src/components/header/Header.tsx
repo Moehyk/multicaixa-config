4a650449aff60ce4a32d9809552640d462eb249a
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-06 10:26:34 +0100

    Update component
    - created AppHeader that, will have a different structure to the normal header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,23 +1,45 @@
 "use client";
 
-import User from "./User";
+import { usePathname } from "next/navigation";
 
+import Authentication from "./Authentication";
 import Image from "next/image";
 
-import logo_dark from "@/assets/logo_dark.svg";
+import logo from "@/assets/logo_light.svg";
+
+function AppHeader() {
+  return (
+    <div className="flex items-center justify-between h-24 mb-4">
+      <Image
+        src={logo}
+        alt="Logo"
+        width={40}
+        height={40}
+        className="object-contain"
+      />
+      <Authentication />
+    </div>
+  );
+}
 
 export default function Header() {
+  const isHome = usePathname() === "/home";
+
+  if (!isHome) {
+    return <AppHeader />;
+  }
+
   return (
-    <header className="fixed inset-x-0 top-0 z-50 w-full flex items-center h-24 bg-accent">
+    <header className="fixed inset-x-0 top-0 z-50 w-full flex items-center h-24 ">
       <div className="container w-full flex items-center justify-between">
         <Image
-          src={logo_dark}
+          src={logo}
           alt="Logo"
           width={40}
           height={40}
           className="object-contain"
         />
-        <User />
+        <Authentication />
       </div>
     </header>
   );
