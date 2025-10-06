012f1c77c06f1ab2de7d15e3bc929f1ccb8d8bd5
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-06 10:37:28 +0100

    Update component
    - no longer a fixed bar, more like a header, but will do something cool with it soon

--- src/components/empresa/EmpresaToolbar.tsx
+++ src/components/empresa/EmpresaToolbar.tsx
@@ -4,70 +4,43 @@ import { mcxDataStore } from "@/context/mcx";
 import { openContextModal } from "@mantine/modals";
 
 import Link from "next/link";
-import { ActionIcon, Tooltip, Button, Paper } from "@mantine/core";
+import { Button } from "@mantine/core";
 import { IconEdit, IconDeviceDesktop } from "@tabler/icons-react";
-import { size } from "zod/v4";
 
-function Toolbar({ children }: React.PropsWithChildren) {
-  return (
-    <div className="fixed left-0 right-0 top-24 z-50">
-      <div className="w-full bg-body/50 backdrop-blur-md drop-shadow-md py-4">
-        <div className="container flex items-center justify-between">
-          {children}
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function EmpresaEditSection() {
+export default function EmpresaToolbar() {
   const { nome } = mcxDataStore();
 
   return (
-    <div className="flex items-center gap-2">
-      <h2 className="text-2xl font-semibold">{nome}</h2>
-      <Tooltip label="Editar Empresa">
-        <ActionIcon
+    <div className="mb-16">
+      <h2 className="text-4xl font-semibold mb-4">{nome}</h2>
+      <div className="flex gap-4">
+        <Button
+          size="md"
+          variant="default"
           component={Link}
           href="/multicaixa/empresa"
-          variant="transparent"
           color="black"
+          rightSection={<IconEdit size={20} />}
         >
-          <IconEdit size={24} />
-        </ActionIcon>
-      </Tooltip>
+          Editar Empresa
+        </Button>
+        <Button
+          size="md"
+          variant="default"
+          onClick={() =>
+            openContextModal({
+              modal: "mcx-modal",
+              size: 1200,
+              innerProps: {
+                type: "DATA",
+              },
+            })
+          }
+          rightSection={<IconDeviceDesktop size={20} />}
+        >
+          Multicaixa
+        </Button>
+      </div>
     </div>
-  );
-}
-
-function McxModalSection() {
-  return (
-    <>
-      <Button
-        size="md"
-        variant="default"
-        onClick={() =>
-          openContextModal({
-            modal: "mcx-modal",
-            size: 1200,
-            innerProps: {
-              type: "DATA",
-            },
-          })
-        }
-        rightSection={<IconDeviceDesktop size={20} />}
-      >
-        Multicaixa
-      </Button>
-    </>
-  );
-}
-
-export default function EmpresaToolbar() {
-  return (
-    <Toolbar>
-      <EmpresaEditSection />
-      <McxModalSection />
-    </Toolbar>
   );
 }
