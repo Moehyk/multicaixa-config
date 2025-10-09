2fe6df28823f131b991ee9fdf73c44dd60d1345e
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-09 07:24:36 +0100

    Config basic custom animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,6 +67,42 @@ export default {
         "brand-text": "rgb(var(--brand-text) / <alpha-value>)",
         paper: "rgb(var(--paper) / <alpha-value>)",
       },
+      animation: {
+        "fade-in": "fadeIn 0.2s ease-in-out",
+        "fade-out": "fadeOut 0.2s ease-in-out",
+        "slide-down": "slideDown 0.3s ease-out",
+        "slide-up": "slideUp 0.3s ease-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        fadeOut: {
+          "0%": { opacity: "1" },
+          "100%": { opacity: "0" },
+        },
+        slideDown: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        slideUp: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0px)",
+          },
+        },
+      },
     },
   },
   plugins: [
