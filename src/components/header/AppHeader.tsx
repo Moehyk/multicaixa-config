71378af2166e19fcd472ecb24ffa3f4f7e80c8d7
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-09 10:58:17 +0100

    Create component
    - header component for /multicaixa route
    
    - created MotionLogoLink wrapper for adding custom animation to LogoLink
    
    - EmpresaDisplayer component will be observed and MotionLogoLink or EmpresaToolbar will be rendered according to observer result

---
+++ src/components/header/AppHeader.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useIntersectionObserver, useRenderWithAnimation } from "@/hooks";
+
+import { LogoLink } from "@/components";
+import AuthDisplayer from "./AuthDisplayer";
+import EmpresaDisplayer from "./EmpresaDisplayer";
+import EmpresaToolbar from "./EmpresaToolbar";
+
+function MotionLogoLink({ isVisible }: { isVisible: boolean }) {
+  const shouldRender = useRenderWithAnimation(isVisible);
+
+  if (!shouldRender) return null;
+
+  return (
+    <div className={`${shouldRender ? "animate-fade-in" : "animate-fade-out"}`}>
+      <LogoLink />
+    </div>
+  );
+}
+
+export default function AppHeader() {
+  const { sentinelRef, intersecting } = useIntersectionObserver({
+    root: null,
+    threshold: 1,
+  });
+
+  return (
+    <>
+      <header className="fixed top-0 left-0 right-0 z-20 h-16 bg-paper/80 flex items-center border-b border-border backdrop-blur-lg drop-shadow-sm">
+        <div className="container w-full flex items-center justify-between">
+          {intersecting && <MotionLogoLink isVisible={true} />}
+          {!intersecting && <EmpresaToolbar isVisible={true} />}
+          <AuthDisplayer />
+        </div>
+      </header>
+      <EmpresaDisplayer ref={sentinelRef} />
+    </>
+  );
+}
