ed9d69287c89cd3c61f049e1228d9fdcd227d5a1
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-13 11:05:28 +0100

    Update component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,9 +3,9 @@ import { Anchor } from "@mantine/core";
 
 export default function Home() {
   return (
-    <div className="flex-1">
+    <div className="flex-1 max-h-dvh bg-blue-500">
       <PageWrapper>
-        <div className="mt-32 ">
+        <div className="mt-32">
           <Anchor href="/multicaixa" className="text-white">
             Multicaixa
           </Anchor>
