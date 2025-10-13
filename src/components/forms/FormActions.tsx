6fd8d34ad02e29639083e38e2d2fb1507e2802a5
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-13 13:12:25 +0100

    Update component
    - PreviewModalTrigger will now be receiving a function

--- src/components/forms/FormActions.tsx
+++ src/components/forms/FormActions.tsx
@@ -1,37 +1,13 @@
-import { useAppPreviewStore } from "@/context/mcx/app-preview-store";
-import { openContextModal } from "@mantine/modals";
-
 import Link from "next/link";
 import { Button } from "@mantine/core";
 import { IconDeviceDesktop } from "@tabler/icons-react";
 
-import type { ProdutoCarregamentoForm } from "@/types";
-
-function PreviewModalTrigger(values: ProdutoCarregamentoForm) {
-  const handleOpenPreviewModal = () => {
-    useAppPreviewStore.setState({
-      produto: {
-        desigEcra: values.desigEcra,
-        desigTeclaSeleccao: values.desigTeclaSeleccao,
-        type: "carregamentos",
-        carregamento: values.carregamento!,
-      },
-    });
-
-    openContextModal({
-      modal: "mcx-modal",
-      size: 1200,
-      innerProps: {
-        type: "PREVIEW",
-      },
-    });
-  };
+function PreviewModalTrigger({ openModal }: { openModal: () => void }) {
   return (
     <Button
-      variant="outline"
       size="md"
       rightSection={<IconDeviceDesktop size={20} />}
-      onClick={handleOpenPreviewModal}
+      onClick={openModal}
     >
       Visualizar
     </Button>
@@ -40,19 +16,19 @@ function PreviewModalTrigger(values: ProdutoCarregamentoForm) {
 
 export default function FormActions({
   isSubmitting,
-  previewValues,
+  openPreviewModal,
   submitText,
 }: {
   isSubmitting: boolean;
   submitText: string;
-  previewValues?: ProdutoCarregamentoForm;
+  openPreviewModal?: () => void;
 }) {
   return (
     <div className="flex gap-2 pt-8">
       <Button component={Link} href="/multicaixa" variant="default" size="md">
         Voltar
       </Button>
-      {previewValues && <PreviewModalTrigger {...previewValues} />}
+      {openPreviewModal && <PreviewModalTrigger openModal={openPreviewModal} />}
       <Button size="md" type="submit" loading={isSubmitting}>
         {submitText}
       </Button>
