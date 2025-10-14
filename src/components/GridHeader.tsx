89cbfe872ee7ae5a01c5377c2b22174c21b3fb18
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-14 13:09:57 +0100

    Update component
    - we won't be using the alternative loader, so no need for checking for id

--- src/components/GridHeader.tsx
+++ src/components/GridHeader.tsx
@@ -4,25 +4,23 @@ import { modals } from "@mantine/modals";
 import { Button } from "@mantine/core";
 import { ServicoModalForm } from "@/components/servico";
 
-export default function GridHeader({ id }: { id?: string }) {
+export default function GridHeader({ id }: { id: string }) {
   return (
     <div className="flex justify-between items-center mb-4 h-9">
       <div className="flex items-stretch gap-2">
         <div className="w-1  bg-brand-accent" />
         <h2 className="text-xl font-semibold">Lista de Serviços</h2>
       </div>
-      {id && (
-        <Button
-          onClick={() =>
-            modals.open({
-              title: "Criar Novo Serviço",
-              children: <ServicoModalForm empresaId={id} />,
-            })
-          }
-        >
-          Novo Serviço
-        </Button>
-      )}
+      <Button
+        onClick={() =>
+          modals.open({
+            title: "Criar Novo Serviço",
+            children: <ServicoModalForm empresaId={id} />,
+          })
+        }
+      >
+        Novo Serviço
+      </Button>
     </div>
   );
 }
