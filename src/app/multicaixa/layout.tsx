2ef82ed8d9b86a6ea6c8dab5e3aabf4a575fb751
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-14 07:59:36 +0100

    Update component
    - removed AppHeader from EmpresaLoader boundary, while maintaining EmpresaDisplayer

--- src/app/multicaixa/layout.tsx
+++ src/app/multicaixa/layout.tsx
@@ -1,17 +1,30 @@
-import { PageWrapper, AppHeader, EmpresaLoader, Footer } from "@/components";
+import { Suspense } from "react";
+import {
+  PageWrapper,
+  AppHeader,
+  EmpresaLoader,
+  Footer,
+  EmpresaDisplayer,
+  GlobalLoader,
+} from "@/components";
 
 export default function MulitcaixaLayout({
   children,
 }: React.PropsWithChildren) {
   return (
-    <EmpresaLoader>
+    <>
       <main className="mt-28 flex-1 overflow-hidden">
         <PageWrapper>
           <AppHeader />
-          {children}
+          <Suspense fallback={<GlobalLoader />}>
+            <EmpresaLoader>
+              <EmpresaDisplayer />
+              {children}
+            </EmpresaLoader>
+          </Suspense>
         </PageWrapper>
       </main>
       <Footer />
-    </EmpresaLoader>
+    </>
   );
 }
