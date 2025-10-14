c304f569520809cf2db46b2c7b4322abd2c38870
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-14 07:55:07 +0100

    Realocate component
    - EmpresaDisplayer won't be rendered as a child of AppHeader (technically it was never a child...), in order to remove the header from the suspense boundary

---
+++ src/components/empresa/EmpresaDisplayer.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { RefObject } from "react";
+import { useEmpresaDisplayer } from "@/hooks";
+
+import Link from "next/link";
+import { Button } from "@mantine/core";
+import { IconEdit, IconDeviceDesktop } from "@tabler/icons-react";
+
+import classes from "./buttons.module.css";
+
+export default function EmpresaDisplayer() {
+  const { nome, openModal } = useEmpresaDisplayer();
+
+  return (
+    <section className="pb-16">
+      <h2 className="text-4xl font-semibold mb-4">{nome}</h2>
+      <div className="flex gap-2 max-w-88">
+        <Button
+          component={Link}
+          href="/multicaixa/empresa"
+          variant="default"
+          rightSection={<IconEdit size={20} />}
+          flex={1}
+          classNames={{
+            root: classes.btn,
+          }}
+        >
+          Editar Empresa
+        </Button>
+        <Button
+          variant="default"
+          onClick={openModal}
+          rightSection={<IconDeviceDesktop size={20} />}
+          flex={1}
+          classNames={{
+            root: classes.btn,
+          }}
+        >
+          Ver Multicaixa
+        </Button>
+      </div>
+    </section>
+  );
+}
--- src/components/header/AppHeader.tsx
+++ src/components/header/AppHeader.tsx
@@ -4,7 +4,6 @@ import { useIntersectionObserver, useRenderWithAnimation } from "@/hooks";
 
 import { LogoLink } from "@/components";
 import AuthDisplayer from "./AuthDisplayer";
-import EmpresaDisplayer from "./EmpresaDisplayer";
 import EmpresaToolbar from "./EmpresaToolbar";
 
 function MotionLogoLink({ isVisible }: { isVisible: boolean }) {
@@ -20,21 +19,18 @@ function MotionLogoLink({ isVisible }: { isVisible: boolean }) {
 }
 
 export default function AppHeader() {
-  const { sentinelRef, intersecting } = useIntersectionObserver({
+  const { intersecting } = useIntersectionObserver({
     root: null,
     threshold: 1,
   });
 
   return (
-    <>
-      <header className="fixed top-0 left-0 right-0 z-20 h-16 bg-paper/50 flex items-center border-b border-border backdrop-blur-xl drop-shadow-sm">
-        <div className="container w-full flex items-center justify-between">
-          {intersecting && <MotionLogoLink isVisible={true} />}
-          {!intersecting && <EmpresaToolbar isVisible={true} />}
-          <AuthDisplayer />
-        </div>
-      </header>
-      <EmpresaDisplayer ref={sentinelRef} />
-    </>
+    <header className="fixed top-0 left-0 right-0 z-20 h-16 bg-paper/50 flex items-center border-b border-border backdrop-blur-xl drop-shadow-sm">
+      <div className="container w-full flex items-center justify-between">
+        {intersecting && <MotionLogoLink isVisible={true} />}
+        {!intersecting && <EmpresaToolbar isVisible={true} />}
+        <AuthDisplayer />
+      </div>
+    </header>
   );
 }
