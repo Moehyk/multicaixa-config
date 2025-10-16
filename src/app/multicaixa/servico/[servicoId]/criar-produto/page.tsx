5a7c27e6d8bef8354d2753799fd4ba8c4aab09e2
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-16 07:16:45 +0100

    Update component
    - no need for fetching data, we'll get servico from EmpresaStore

--- src/app/multicaixa/servico/[servicoId]/criar-produto/page.tsx
+++ src/app/multicaixa/servico/[servicoId]/criar-produto/page.tsx
@@ -1,7 +1,3 @@
-import { getServico } from "@/server/services";
-import { notFound } from "next/navigation";
-
-import { FormCard } from "@/components/forms";
 import { CriarProduto } from "@/components/produto";
 
 export default async function CriarProdutoPage({
@@ -11,15 +7,5 @@ export default async function CriarProdutoPage({
 }) {
   const { servicoId } = await params;
 
-  const { data } = await getServico(servicoId);
-
-  if (!data) {
-    notFound();
-  }
-
-  return (
-    <FormCard header={data.desigSistema} subheader="Criar Produto">
-      <CriarProduto id={servicoId} />
-    </FormCard>
-  );
+  return <CriarProduto id={servicoId} />;
 }
--- src/components/produto/CriarProduto.tsx
+++ src/components/produto/CriarProduto.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { notFound } from "next/navigation";
+import { mcxEmpresaStore } from "@/context/mcx";
+
+import { FormCard } from "../forms";
 import CreatePagamento from "./CreatePagamento";
 import CreateCarregamento from "./CreateCarregamento";
 import CreateRecargas from "./CreateRecargas";
@@ -8,28 +12,38 @@ import { Tabs } from "@mantine/core";
 import styles from "./tabs.module.css";
 
 export default function CriarProdutoForm({ id }: { id: string }) {
+  const { getServico } = mcxEmpresaStore();
+
+  const servico = getServico(id);
+
+  if (!servico) {
+    notFound();
+  }
+
   return (
-    <Tabs defaultValue="pagamento" variant="pills">
-      <Tabs.List grow>
-        <Tabs.Tab value="pagamento" className={styles.tabs}>
-          Pagamento
-        </Tabs.Tab>
-        <Tabs.Tab value="carregamentos" className={styles.tabs}>
-          Carregamentos
-        </Tabs.Tab>
-        <Tabs.Tab value="recargas" className={styles.tabs}>
-          Recargas
-        </Tabs.Tab>
-      </Tabs.List>
-      <Tabs.Panel value="pagamento">
-        <CreatePagamento servicoId={id} />
-      </Tabs.Panel>
-      <Tabs.Panel value="carregamentos">
-        <CreateCarregamento servicoId={id} />
-      </Tabs.Panel>
-      <Tabs.Panel value="recargas">
-        <CreateRecargas servicoId={id} />
-      </Tabs.Panel>
-    </Tabs>
+    <FormCard header={servico.desigEcra} subheader="Criar Produto">
+      <Tabs defaultValue="pagamento" variant="pills">
+        <Tabs.List grow>
+          <Tabs.Tab value="pagamento" className={styles.tabs}>
+            Pagamento
+          </Tabs.Tab>
+          <Tabs.Tab value="carregamentos" className={styles.tabs}>
+            Carregamentos
+          </Tabs.Tab>
+          <Tabs.Tab value="recargas" className={styles.tabs}>
+            Recargas
+          </Tabs.Tab>
+        </Tabs.List>
+        <Tabs.Panel value="pagamento">
+          <CreatePagamento servicoId={id} />
+        </Tabs.Panel>
+        <Tabs.Panel value="carregamentos">
+          <CreateCarregamento servicoId={id} />
+        </Tabs.Panel>
+        <Tabs.Panel value="recargas">
+          <CreateRecargas servicoId={id} />
+        </Tabs.Panel>
+      </Tabs>
+    </FormCard>
   );
 }
