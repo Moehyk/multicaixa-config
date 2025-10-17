ee4c5bb12ac5a1580c6b641df39d0cc90d08a27b
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-17 13:06:04 +0100

    Update component
    - silly text rendering, because if there's no empresa this component will not be rendered

--- src/components/multicaixa/McxScreenText.tsx
+++ src/components/multicaixa/McxScreenText.tsx
@@ -1,11 +1,13 @@
 import { mcxEmpresaStore } from "@/context/mcx";
 
 export default function McxScreenText({ subtext }: { subtext?: string }) {
-  const { desigEcra } = mcxEmpresaStore();
+  const { empresa } = mcxEmpresaStore();
 
   return (
     <div className="text-center uppercase text-white flex-1 pt-8">
-      <h1 className="text-4xl font-bold">{desigEcra}</h1>
+      <h1 className="text-4xl font-bold">
+        {empresa ? empresa.desigEcra : "[Designação Empresa]"}
+      </h1>
       <h2 className="text-2xl font-medium mt-4">{subtext}</h2>
     </div>
   );
