7f4a0f49dc2a92765ce9440a3bd3250de9f3e94b
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-17 15:35:17 +0100

    Update component
    - added fixed to center the component at the middle of the page, don't care if it is lame...
    
    - restructure card elements

--- src/components/empresa/NoEmpresa.tsx
+++ src/components/empresa/NoEmpresa.tsx
@@ -8,33 +8,41 @@ import { IconHomePlus } from "@tabler/icons-react";
 
 export default function NoEmpresa() {
   return (
-    <Paper withBorder w={"max-content"} p={48} className="mx-auto mt-10">
-      <div className="flex items-center mb-8">
-        <div className="bg-accent w-1 h-12 mr-4" />
-        <span className="font-medium">
-          <p>
-            Bem vindo ao&nbsp;
-            <span className="font-bold text-[var(--mantine-color-brand-text)]">
-              MCX Config
-            </span>
-            &#46;
-          </p>
-          <p>Para começar deve criar a sua empresa.</p>
-        </span>
-      </div>
-      <Button
-        onClick={() =>
-          modals.open({
-            title: "Criar Empresa",
-            size: "xl",
-            children: <CriarEmpresaModal />,
-            withCloseButton: true,
-          })
-        }
-        leftSection={<IconHomePlus size={20} />}
+    <div className="fixed top-1/2 left-0 right-0 -translate-y-1/2 z-50">
+      <Paper
+        withBorder
+        w={"max-content"}
+        p={40}
+        className="m-auto content-center"
       >
-        Criar Empresa
-      </Button>
-    </Paper>
+        <div className="flex items-center gap-16">
+          <div className="flex items-center">
+            <div className="bg-brand-accent w-1 h-16 mr-4" />
+            <span className="font-medium">
+              <p className="text-2xl pb-2">
+                Bem vindo ao&nbsp;
+                <span className="font-bold text-[var(--mantine-color-brand-text)]">
+                  McxConfig
+                </span>
+              </p>
+              <p>Para começar deve criar a sua empresa</p>
+            </span>
+          </div>
+          <Button
+            onClick={() =>
+              modals.open({
+                title: "Criar Empresa",
+                size: "xl",
+                children: <CriarEmpresaModal />,
+                withCloseButton: true,
+              })
+            }
+            leftSection={<IconHomePlus size={20} />}
+          >
+            Criar Empresa
+          </Button>
+        </div>
+      </Paper>
+    </div>
   );
 }
