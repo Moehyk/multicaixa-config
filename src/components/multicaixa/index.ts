ba0461b3f42abe8b68223350b50365b4125e7735
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-20 14:27:14 +0100

    Fix imports

--- src/components/multicaixa/index.ts
+++ src/components/multicaixa/index.ts
@@ -7,7 +7,3 @@ export { default as McxEndView } from "./McxEndView";
 export { default as McxServicoPreView } from "./McxServicoPreView";
 export { default as McxProdutoPreView } from "./McxProdutoPreView";
 export { default as McxModal } from "./McxModal";
-<<<<<<< HEAD
-=======
-export { default as McxRecibo } from "./McxRecibo";
->>>>>>> 02df6c6f22601cd6300d9ac7f1c1f4d2d4a6d62e
