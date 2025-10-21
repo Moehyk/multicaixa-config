1493562fd354e054fe9549b9d0b6b1ae6a2e1d01
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-21 13:00:56 +0100

    Update component
    - updated prop, will now only render a message
    
    - Button component will render a Link component instead of using useRouter

--- src/components/ErrorWidget.tsx
+++ src/components/ErrorWidget.tsx
@@ -1,24 +1,25 @@
-"use client";
-
-import { useErrorWidget } from "@/hooks/error-widget";
+import Link from "next/link";
 
 import { Button, ThemeIcon, Paper } from "@mantine/core";
 import { IconExclamationCircle } from "@tabler/icons-react";
 
 export default function ErroWidget({
-  error,
+  message,
 }: {
-  error: Error & { digest?: string };
+  message: string | undefined;
 }) {
-  const back = useErrorWidget(error);
-
   return (
     <Paper w="50%" p={32} withBorder className="mb-32 items-center self-center">
       <ThemeIcon variant="transparent" color="red" size={64}>
         <IconExclamationCircle size={64} />
       </ThemeIcon>
-      <h2 className="font-semibold text-lg">Aconteceu algo de errado!</h2>
-      <Button variant="default" className="w-max mt-8" onClick={back}>
+      <h2 className="font-semibold text-lg">{message}</h2>
+      <Button
+        component={Link}
+        href="/multicaixa"
+        variant="default"
+        className="w-max mt-8"
+      >
         Voltar
       </Button>
     </Paper>
