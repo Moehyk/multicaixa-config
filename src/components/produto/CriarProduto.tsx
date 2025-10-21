a622b457b537eee831b151e0517df3c32842da52
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-21 13:10:33 +0100

    Update component
    - we won't be getting stuff from store anymore, so passing everything through prop drilling

--- src/components/produto/CriarProduto.tsx
+++ src/components/produto/CriarProduto.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import { notFound } from "next/navigation";
-import { mcxEmpresaStore } from "@/context/mcx";
-
 import { FormCard } from "../forms";
 import CreatePagamento from "./CreatePagamento";
 import CreateCarregamento from "./CreateCarregamento";
@@ -11,17 +8,15 @@ import { Tabs } from "@mantine/core";
 
 import styles from "./tabs.module.css";
 
-export default function CriarProdutoForm({ id }: { id: string }) {
-  const { getServico } = mcxEmpresaStore();
-
-  const servico = getServico(id);
-
-  if (!servico) {
-    notFound();
-  }
-
+export default function CriarProdutoForm({
+  headerText,
+  id,
+}: {
+  headerText: string;
+  id: string;
+}) {
   return (
-    <FormCard header={servico.desigEcra} subheader="Criar Produto">
+    <FormCard header={headerText} subheader="Criar Produto">
       <Tabs defaultValue="pagamento" variant="pills">
         <Tabs.List grow>
           <Tabs.Tab value="pagamento" className={styles.tabs}>
