fea4052382fb9d9a45bd43d4eeb05ff7c08c7fb2
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-21 16:19:13 +0100

    Update component
    - updated component to manage error boundary

--- src/components/ErrorWidget.tsx
+++ src/components/ErrorWidget.tsx
@@ -1,25 +1,18 @@
-import Link from "next/link";
+"use client";
 
 import { Button, ThemeIcon, Paper } from "@mantine/core";
 import { IconExclamationCircle } from "@tabler/icons-react";
 
-export default function ErroWidget({
-  message,
-}: {
-  message: string | undefined;
-}) {
+import type { ErrorBoundaryProps } from "@/types";
+
+export default function ErroWidget({ error, reset }: ErrorBoundaryProps) {
   return (
     <Paper w="50%" p={32} withBorder className="mb-32 items-center self-center">
       <ThemeIcon variant="transparent" color="red" size={64}>
         <IconExclamationCircle size={64} />
       </ThemeIcon>
-      <h2 className="font-semibold text-lg">{message}</h2>
-      <Button
-        component={Link}
-        href="/multicaixa"
-        variant="default"
-        className="w-max mt-8"
-      >
+      <h2 className="font-semibold text-lg">{error.message}</h2>
+      <Button variant="default" className="w-max mt-8" onClick={reset}>
         Voltar
       </Button>
     </Paper>
