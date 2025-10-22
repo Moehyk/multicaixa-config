f3ddb0d02785f73bc89bad534baeea59bd905c64
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-22 16:20:44 +0100

    Update component
    - for /multicaixa nested routes we won't be fetching Empresa, so we won't have Empresa.desigEcra to render on McxAppPreview, so we're getting it from the local storage

--- src/components/multicaixa/McxScreenText.tsx
+++ src/components/multicaixa/McxScreenText.tsx
@@ -1,12 +1,17 @@
-import { mcxEmpresaStore } from "@/context/mcx";
+import { useMcxData } from "@/hooks/mcx-data";
+import { useLocalStorage } from "@mantine/hooks";
 
 export default function McxScreenText({ subtext }: { subtext?: string }) {
-  const { empresa } = mcxEmpresaStore();
+  const { empresa } = useMcxData();
+  const [empresaNome] = useLocalStorage<string | null>({
+    key: "empresa",
+    defaultValue: null,
+  });
 
   return (
     <div className="text-center uppercase text-white flex-1 pt-8">
       <h1 className="text-4xl font-bold">
-        {empresa ? empresa.desigEcra : "[Designação Empresa]"}
+        {empresa ? empresa.desigEcra : empresaNome}
       </h1>
       <h2 className="text-2xl font-medium mt-4">{subtext}</h2>
     </div>
