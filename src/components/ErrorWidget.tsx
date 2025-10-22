24bd25d263fe89dd48dcb59d8215efbf68efb158
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-22 10:45:20 +0100

    Update component
    - updating structure

--- src/components/ErrorWidget.tsx
+++ src/components/ErrorWidget.tsx
@@ -7,11 +7,13 @@ import type { ErrorBoundaryProps } from "@/types";
 
 export default function ErroWidget({ error, reset }: ErrorBoundaryProps) {
   return (
-    <Paper w="50%" p={32} withBorder className="mb-32 items-center self-center">
-      <ThemeIcon variant="transparent" color="red" size={64}>
-        <IconExclamationCircle size={64} />
-      </ThemeIcon>
-      <h2 className="font-semibold text-lg">{error.message}</h2>
+    <Paper w="50%" p={16} withBorder className="mb-32 items-center self-center">
+      <div className="flex items-center gap-4">
+        <ThemeIcon variant="transparent" color="red" size={48}>
+          <IconExclamationCircle size={48} />
+        </ThemeIcon>
+        <h2 className="font-medium text-lg">{error.message}</h2>
+      </div>
       <Button variant="default" className="w-max mt-8" onClick={reset}>
         Voltar
       </Button>
