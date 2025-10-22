29389fa9626efe6650d5c2e1ea469b90240afec0
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-22 11:07:46 +0100

    Update exports

--- src/components/produto/index.ts
+++ src/components/produto/index.ts
@@ -2,5 +2,5 @@ export { default as CriarProduto } from "./CriarProduto";
 export { default as UpdatePagamento } from "./UpdatePagamento";
 export { default as UpdateRecargas } from "./UpdateRecargas";
 export { default as UpdateCarregamento } from "./UpdateCarregamento";
-export { default as GridProdutos } from "./GridProdutos";
 export { default as GridNoProduto } from "./GridNoProduto";
+export { default as ProdutoItem } from "./ProdutoItem";
