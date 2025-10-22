9c8bc5522870cef8b95b11f6bd1ab9c5ddabdacf
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-22 10:34:00 +0100

    Update component
    - and now we're setting the search params as page server component prop, extracting it, transform it with the util function and passing it to FormCard
    
    - clean and simple

--- src/app/multicaixa/produto/[...slug]/page.tsx
+++ src/app/multicaixa/produto/[...slug]/page.tsx
@@ -1,15 +1,16 @@
 import { getProduto } from "@/server/services";
 import { notFound } from "next/navigation";
-import { sortDataArray } from "@/utils/arrays";
+import { sortDataArray, renderServicoNomeParam } from "@/utils";
 
 import {
-  ErrorWidget,
+  FormCard,
   UpdatePagamento,
   UpdateRecargas,
   UpdateCarregamento,
 } from "@/components";
 
 import type { ProdutoData } from "@/types";
+import type { SearchParams } from "nuqs";
 
 const renderProduto = (data: ProdutoData) => {
   switch (data.type) {
@@ -30,10 +31,13 @@ const renderProduto = (data: ProdutoData) => {
 
 export default async function DynamicProdutoPage({
   params,
+  searchParams,
 }: {
   params: Promise<{ slug: string[] }>;
+  searchParams: Promise<SearchParams>;
 }) {
   const { slug } = await params;
+  const { s } = await searchParams;
 
   const { data } = await getProduto(slug[0]);
 
@@ -41,5 +45,11 @@ export default async function DynamicProdutoPage({
     notFound();
   }
 
-  return <>{renderProduto(data)}</>;
+  const servicoDesigSistema = renderServicoNomeParam(s);
+
+  return (
+    <FormCard header={servicoDesigSistema} subheader={data.desigEcra}>
+      {renderProduto(data)}
+    </FormCard>
+  );
 }
