1ebfcf94535f0b8b4cb0f9265ddd540a98838f76
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-23 16:03:12 +0100

    Update component
    - switching from local storage to url search params access

--- src/components/multicaixa/McxScreenText.tsx
+++ src/components/multicaixa/McxScreenText.tsx
@@ -1,17 +1,16 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
 import { useMcxData } from "@/hooks/mcx-data";
-import { useLocalStorage } from "@mantine/hooks";
 
 export default function McxScreenText({ subtext }: { subtext?: string }) {
   const { empresa } = useMcxData();
-  const [empresaNome] = useLocalStorage<string | null>({
-    key: "empresa",
-    defaultValue: null,
-  });
+  const empresaDesigEcra = useSearchParams().get("e") || "[MINHA EMPRESA]";
 
   return (
     <div className="text-center uppercase text-white flex-1 pt-8">
       <h1 className="text-4xl font-bold">
-        {empresa ? empresa.desigEcra : empresaNome}
+        {empresa ? empresa.desigEcra : empresaDesigEcra}
       </h1>
       <h2 className="text-2xl font-medium mt-4">{subtext}</h2>
     </div>
