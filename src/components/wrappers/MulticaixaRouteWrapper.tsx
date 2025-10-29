c8998f708a4710cca6ceb616b24d62a0da0eec2e
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-29 10:36:59 +0100

    Update component
    - expanding root wrapper to MulticaixaRouteWrapper

--- src/components/wrappers/MulticaixaRouteWrapper.tsx
+++ src/components/wrappers/MulticaixaRouteWrapper.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 
-export default function PageWrapper({ children }: React.PropsWithChildren) {
+export default function MulticaixaRouteWrapper({
+  children,
+}: React.PropsWithChildren) {
   return (
-    <div className="flex-1 mt-28 overflow-hidden">
-      <div className="container flex flex-col">{children}</div>
+    <div className="flex flex-col flex-1">
+      <div className="flex flex-col-fluid flex-row ">
+        <div className="flex-row-fluid relative flex min-h-dvh flex-col">
+          <div className="flex-1 mt-28 overflow-hidden">
+            <div className="container flex flex-col">{children}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
