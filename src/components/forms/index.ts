362d2222ed47cbbd8368d548bbbddb1308ca841d
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-30 15:51:33 +0100

    Update exports

--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -1,4 +1,5 @@
 export { default as FormCard } from "./FormCard";
+export { default as FormActions } from "./FormActions";
 export { default as PagamentoForm } from "./PagamentoForm";
 export { default as RecargasForm } from "./RecargasForm";
 export { default as CarregamentoForm } from "./CarregamentoForm";
