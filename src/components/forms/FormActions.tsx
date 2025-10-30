ac68dbff9ca25ae1df901200513117ddd844edaa
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-30 14:29:55 +0100

    Update component
    - FormActions will now be a compound component that will render the diferent type of actions

--- src/components/forms/FormActions.tsx
+++ src/components/forms/FormActions.tsx
@@ -1,32 +1,58 @@
 import Link from "next/link";
 import { Button } from "@mantine/core";
 import { IconDeviceDesktop } from "@tabler/icons-react";
+import { Form } from "@mantine/form";
 
-export default function FormActions({
-  openPreviewModal,
-  submitText,
-}: {
-  submitText: string;
-  openPreviewModal?: () => void;
-}) {
+function VoltarAction() {
   return (
-    <div className="flex gap-2 pt-8">
-      <Button component={Link} href="/multicaixa" variant="default" size="md">
-        Voltar
-      </Button>
-      {openPreviewModal && (
-        <Button
-          variant="light"
-          size="md"
-          rightSection={<IconDeviceDesktop size={20} />}
-          onClick={openPreviewModal}
-        >
-          Visualizar
-        </Button>
-      )}
-      <Button size="md" type="submit">
-        {submitText}
-      </Button>
-    </div>
+    <Button component={Link} href="/multicaixa" variant="default" size="md">
+      Voltar
+    </Button>
   );
 }
+
+function VisualizarAction({
+  openPreviewModal,
+}: {
+  openPreviewModal: () => void;
+}) {
+  return (
+    <Button
+      variant="light"
+      size="md"
+      rightSection={<IconDeviceDesktop size={20} />}
+      onClick={openPreviewModal}
+    >
+      Visualizar
+    </Button>
+  );
+}
+
+function CriarAction() {
+  return (
+    <Button size="md" type="submit">
+      Criar
+    </Button>
+  );
+}
+
+function EditarAction() {
+  return (
+    <Button size="md" type="submit">
+      Editar
+    </Button>
+  );
+}
+
+export default function FormActions({
+  children,
+}: {
+  children?: React.ReactNode;
+}) {
+  return <div className="flex gap-2 pt-8">{children}</div>;
+}
+
+FormActions.Voltar = VoltarAction;
+FormActions.Visualizar = VisualizarAction;
+FormActions.Criar = CriarAction;
+FormActions.Editar = EditarAction;
