31976b3eea69374de49eef16560c9ec5c08c06fb
Ruben Silva <rubendacostaesilva@gmail.com>
2025-10-30 15:34:40 +0100

    Update component
    - updates positioning styles
    
    - replaced Image for LogoLink

--- src/components/header/HomeHeader.tsx
+++ src/components/header/HomeHeader.tsx
@@ -1,19 +1,11 @@
 import AuthDisplayer from "./AuthDisplayer";
-import Image from "next/image";
-
-import logo from "@/assets/logo_light.svg";
+import { LogoLink } from "@/components";
 
 export default function HomeHeader() {
   return (
-    <header className="fixed inset-x-0 top-0 z-50 w-full flex items-center h-24 ">
+    <header className="w-full flex items-center h-20">
       <div className="container w-full flex items-center justify-between">
-        <Image
-          src={logo}
-          alt="Logo"
-          width={40}
-          height={40}
-          className="object-contain"
-        />
+        <LogoLink />
         <AuthDisplayer />
       </div>
     </header>
