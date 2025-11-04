31256ce44fcfd5fc950076929c2c4ff893397aef
Ruben Silva <rubendacostaesilva@gmail.com>
2025-11-04 11:33:05 +0100

    Update component
    - refactor hero section

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,15 +1,12 @@
-import { Anchor } from "@mantine/core";
+"use client";
+
+import { HomeHeader, HomeHeroSection } from "@/components";
 
 export default function Home() {
   return (
-    <div className="flex-1 max-h-dvh bg-blue-500">
-      <div className="container flex flex-col">
-        <div className="mt-32">
-          <Anchor href="/multicaixa" className="text-white">
-            Multicaixa
-          </Anchor>
-        </div>
-      </div>
-    </div>
+    <>
+      <HomeHeader />
+      <HomeHeroSection />
+    </>
   );
 }
