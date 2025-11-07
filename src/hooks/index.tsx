8290824161e7e2a334327d46e710fa784c0a3f86
Ruben Silva <rubendacostaesilva@gmail.com>
2025-11-07 09:39:53 +0100

    Delete hook
    - we're not using local storage anymore so no need for this hook

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,5 +1,4 @@
 export { useInputSeparators } from "./useInputSeparators";
 export { useMcxCustomInput } from "./useMcxCustomInput";
-export { useEmpresaDisplayer } from "./empresa-displayer";
 export { useIntersectionObserver } from "./intersection-observer";
 export { useRenderWithAnimation } from "./render-with-animation";
