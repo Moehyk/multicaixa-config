1113887428683bddedfc72d925bab32efb30ee52
Ruben Silva <rubendacostaesilva@gmail.com>
2025-12-03 20:29:15 +0100

    Update store
    - updated store value type

--- src/context/empresa-displayer-ref.ts
+++ src/context/empresa-displayer-ref.ts
@@ -2,7 +2,7 @@ import { RefObject } from "react";
 import { create } from "zustand";
 
 export const empresaDisplayerRefStore = create<{
-  ref: RefObject<HTMLElement | null>;
+  ref: RefObject<HTMLDivElement | null>;
 }>(() => ({
   ref: { current: null },
 }));
