081ad797cc7049f858511fe44fb29442dc105b3e
Ruben Silva <rubendacostaesilva@gmail.com>
2025-12-03 20:37:15 +0100

    Update component
    - change component structure, we'll now be rendering a paper wrapper, with empresa nome on the right side, and in the left side a set of actions
    
    - added new action for creating servico, that was previously on the deleted GridHeader component
    
    - added logic for only rendering criar servico and editar empresa actions when in "/multicaixa" route, because on other routes there will be no sense having them

--- src/components/empresa/EmpresaDisplayer.tsx
+++ src/components/empresa/EmpresaDisplayer.tsx
@@ -1,51 +1,91 @@
 "use client";
 
 import { useEffect, useRef } from "react";
+import { usePathname } from "next/navigation";
 import { empresaDisplayerRefStore } from "@/context/empresa-displayer-ref";
+import { modals } from "@mantine/modals";
 import { openMcxDataModal } from "@/utils";
 
 import Link from "next/link";
-import { Button } from "@mantine/core";
+import { ServicoModalForm } from "@/components/servico";
+import { Button, Paper } from "@mantine/core";
 import { IconEdit, IconDeviceDesktop } from "@tabler/icons-react";
 
-import classes from "./buttons.module.css";
 import { EmpresaData } from "@/types";
 
+function OpenMcxDataModal({
+  data,
+  flex,
+}: {
+  data: EmpresaData;
+  flex?: number;
+}) {
+  return (
+    <Button
+      flex={flex}
+      variant="default"
+      onClick={() => openMcxDataModal(data)}
+      rightSection={<IconDeviceDesktop size={20} />}
+    >
+      Ver Multicaixa
+    </Button>
+  );
+}
+
+function DisplayerActions({ data }: { data: EmpresaData }) {
+  const pathname = usePathname();
+
+  return (
+    <>
+      {pathname === "/multicaixa" ? (
+        <div className="flex gap-2 w-1/2">
+          <OpenMcxDataModal data={data} flex={1} />
+          <Button
+            component={Link}
+            href="/multicaixa/empresa"
+            variant="default"
+            rightSection={<IconEdit size={20} />}
+            flex={1}
+          >
+            Editar Empresa
+          </Button>
+
+          <Button
+            flex={1}
+            onClick={() =>
+              modals.open({
+                title: "Criar Novo Serviço",
+                children: <ServicoModalForm empresaId={data.id} />,
+              })
+            }
+          >
+            Novo Serviço
+          </Button>
+        </div>
+      ) : (
+        <OpenMcxDataModal data={data} />
+      )}
+    </>
+  );
+}
+
 export default function EmpresaDisplayer({ data }: { data: EmpresaData }) {
-  const sentinelRef = useRef<HTMLElement>(null);
+  const sentinelRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     empresaDisplayerRefStore.setState({ ref: sentinelRef });
   }, [sentinelRef]);
 
   return (
-    <section ref={sentinelRef} className="pb-16">
-      <h2 className="text-4xl font-semibold mb-4">{data.nome}</h2>
-      <div className="flex gap-2 max-w-88">
-        <Button
-          component={Link}
-          href="/multicaixa/empresa"
-          variant="default"
-          rightSection={<IconEdit size={20} />}
-          flex={1}
-          classNames={{
-            root: classes.btn,
-          }}
-        >
-          Editar Empresa
-        </Button>
-        <Button
-          variant="default"
-          onClick={() => openMcxDataModal(data)}
-          rightSection={<IconDeviceDesktop size={20} />}
-          flex={1}
-          classNames={{
-            root: classes.btn,
-          }}
-        >
-          Ver Multicaixa
-        </Button>
-      </div>
-    </section>
+    <Paper
+      withBorder
+      p="lg"
+      mb={32}
+      ref={sentinelRef}
+      className="flex items-center justify-between"
+    >
+      <h2 className="text-2xl font-semibold max-w-100">{data.nome}</h2>
+      <DisplayerActions data={data} />
+    </Paper>
   );
 }
