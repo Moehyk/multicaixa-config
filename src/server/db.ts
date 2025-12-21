abc022ea40a91a48b72b303538b5abe9e697d72e
Ruben Silva <rubendacostaesilva@gmail.com>
2025-12-21 18:44:05 +0100

    Update prisma

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,17 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import dotenv from "dotenv";
 
 declare global {
   var cachedPrisma: PrismaClient;
 }
 
-let prisma: PrismaClient;
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient();
-  }
-  prisma = global.cachedPrisma;
-}
+dotenv.config();
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaNeon({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
 export const db = prisma;
